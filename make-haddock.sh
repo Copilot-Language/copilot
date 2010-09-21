7ae5ea40dacd405c4453e3cefb8e33b4974dbfb1
Lee Pike <leepike@gmail.com>
2010-09-20 22:52:39 -0700

    Shell script for generating Haddock documentation.

--- make-haddock.sh
+++ make-haddock.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Make documentation using Haddock and move to doc/
 
 cabal haddock --haddock-option=--source-base=http://github.com/leepike/Copilot --haddock-option=--source-module=http://github.com/leepike/Copilot/%M.html
+cp dist/doc/html/copilot/* doc/
