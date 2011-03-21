f3b01cccffc9309a472d2b50b61d001950c30d43
leepike@gmail.com <leepike@galois.com>
2011-03-20 17:03:19 -0700

    Cleaned up formatting and functions.

--- Language/Copilot/Examples/RegExpExample.hs
+++ Language/Copilot/Examples/RegExpExample.hs
@@ -1,20 +1,20 @@
-module Main where
+module Language.Copilot.Examples.RegExpExample where
 
 import Language.Copilot.Core
 import qualified Language.Copilot.Language as C
 import Language.Copilot.Interface
 import Language.Copilot.Libs.RegExp ( copilotRegexp )
 
+testRegExp :: Streams
+testRegExp = do 
+  let input  = C.varI64 "input"
+      output = C.varB   "output"
+      reset  = C.varB   "reset"
+  input C..= [ 0, 1, 2 ] C.++ (-3)
+  reset C..= [ True ] C.++ C.false
+  copilotRegexp input "<0><1><2>*|<-3>+" output reset
 
-testRegExp = do { let input  = C.varI64 "input"
-                      output = C.varB   "output"
-                      reset  = C.varB   "reset"
-                ; input C..= [ 0, 1, 2 ] C.++ Const (-3)
-                ; reset C..= [ True ] C.++ Const False
-                ; copilotRegexp input "<0><1><2>*|<-3>+" output reset
-                }
-
+main :: IO ()
 main = do
   interpret testRegExp 10 baseOpts
   compile testRegExp "testRegExp" baseOpts
-  return ()
