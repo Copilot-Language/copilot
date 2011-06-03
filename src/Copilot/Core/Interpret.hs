5e084fbe610ce3e8b3738e890c32cd9e00984bcf
Nis Wegmann <niswegmann@gmail.com>
2011-06-03 14:56:17 -0400

    Updated the interpreter to use triggers with multiple arguments.

--- src/Copilot/Core/Interpret.hs
+++ src/Copilot/Core/Interpret.hs
@@ -6,10 +6,11 @@
 {-# LANGUAGE Rank2Types #-}
 
 module Copilot.Core.Interpret
-  ( interpret
+  ( Env
+  , interpret
   ) where
 
-import Data.List (transpose)
+import Data.List (intersperse, transpose)
 import Copilot.Core
 import Copilot.Core.Type.Dynamic
 import Prelude hiding (id)
@@ -17,18 +18,11 @@ import qualified Prelude as P
 import Text.PrettyPrint.NCol (asColumns)
 import Text.PrettyPrint (Doc, text, render)
 
---
+--------------------------------------------------------------------------------
 
-type Env = [(Int, DynamicF [] Type)]
+type Env k = [(k, DynamicF [] Type)]
 
---
-
-newtype EvalExpr α         = EvalExpr { evalExpr :: Env -> [α] }
-newtype EvalOp1  α β       = EvalOp1  { evalOp1  :: α -> β }
-newtype EvalOp2  α β γ     = EvalOp2  { evalOp2  :: α -> β -> γ }
-newtype EvalOp3  α β γ δ   = EvalOp3  { evalOp3  :: α -> β -> γ -> δ }
-
---
+--------------------------------------------------------------------------------
 
 class Strict f where
   strict :: f α -> f α
@@ -37,96 +31,140 @@ instance Strict [] where
   strict []     = []
   strict (x:xs) = x `seq` (x : strict xs)
 
+--------------------------------------------------------------------------------
+
+class Applicative f where
+  pure  :: α -> f α
+  (<*>) :: f (α -> β) -> f α -> f β
+
+--------------------------------------------------------------------------------
+
+newtype EvalExpr α = EvalExpr { evalExpr_ :: Env Name -> Env Id -> [α] }
+
+instance Applicative EvalExpr where
+  pure x = EvalExpr $ \ _ _ -> repeat x
+  e1 <*> e2 = EvalExpr $ \ exts strms ->
+    zipWith ($) (evalExpr_ e1 exts strms) (evalExpr_ e2 exts strms)
+
 instance Strict EvalExpr where
-  strict e = EvalExpr $ \ m -> strict (evalExpr e m)
-
--- EvalExpr is an applicative functor:
-
-pure :: α -> EvalExpr α
-pure x = EvalExpr $ \ _ -> repeat x
-
-(<*>) :: EvalExpr (α -> β) -> EvalExpr α -> EvalExpr β
-e1 <*> e2 = EvalExpr $ \ env -> zipWith ($) (evalExpr e1 env) (evalExpr e2 env)
-
---
+  strict e = EvalExpr $ \ exts strms -> strict (evalExpr_ e exts strms)
 
 instance Expr EvalExpr where
-  const _ x          = x `seq` pure x
-  drop t i id        = strict $ EvalExpr $ \ env ->
-                         case lookup id env >>= fromDynamicF t of
-                           Just xs -> P.drop (fromIntegral i) xs
-                           Nothing -> error "interpret: drop!"
-  extern _ _         = error "interpret: extern!"
-  op1 op e1          = strict $ pure (evalOp1 op) <*> e1
-  op2 op e1 e2       = strict $ pure (evalOp2 op) <*> e1 <*> e2
-  op3 op e1 e2 e3    = strict $ pure (evalOp3 op) <*> e1 <*> e2 <*> e3
+  const _ x       = x `seq` pure x
+  drop t i id     = strict $ EvalExpr $ \ _ strms ->
+                      let Just xs = lookup id strms >>= fromDynamicF t
+                      in P.drop (fromIntegral i) xs
+  extern t name   = strict $ EvalExpr $ \ exts _ -> evalExtern t name exts
+  op1 op e1       = strict $ pure op <*> e1
+  op2 op e1 e2    = strict $ pure (apply2 op) <*> e1 <*> e2
+  op3 op e1 e2 e3 = strict $ pure (apply3 op) <*> e1 <*> e2 <*> e3
 
-instance Op1 EvalOp1 where
-  not      = EvalOp1 P.not
-  abs _    = EvalOp1 P.abs
-  sign _   = EvalOp1 signum
+evalExtern :: Type α -> Name -> Env Name -> [α]
+evalExtern t name exts =
+  case lookup name exts of
+    Nothing -> error $ "Undefined external variable: " ++ name
+    Just dyn ->
+      case fromDynamicF t dyn of
+        Nothing -> error $ "Ill-typed external variable: " ++ name
+        Just xs -> xs
 
-instance Op2 EvalOp2 where
-  and      = EvalOp2 (&&)
-  or       = EvalOp2 (||)
-  add _    = EvalOp2 (+)
-  sub _    = EvalOp2 (-)
-  mul _    = EvalOp2 (*)
-  mod _    = EvalOp2 P.mod
-  div _    = EvalOp2 P.div
-  eq _     = EvalOp2 (==)
-  ne _     = EvalOp2 (/=)
-  le _     = EvalOp2 (<=)
-  ge _     = EvalOp2 (>=)
-  lt _     = EvalOp2 (<)
-  gt _     = EvalOp2 (>)
+evalExpr :: Env Name -> Env Id -> (forall e . Expr e => e α) -> [α]
+evalExpr exts strms (EvalExpr f) = f exts strms
 
-instance Op3 EvalOp3 where
-  mux _    = EvalOp3 $ \ v x y -> if v then x else y
+--------------------------------------------------------------------------------
 
---
+instance Op1 (->) where
+  not      = P.not
+  abs _    = P.abs
+  sign _   = P.signum
 
-type Output = (Bool, String)
+--------------------------------------------------------------------------------
 
-withGuard :: α -> [Bool] -> [α] -> [α]
-withGuard _ (True:vs)  (x:xs) = x : withGuard x vs xs
-withGuard z (False:vs) xs     = z : withGuard z vs xs
-withGuard _ _          _      = error "withGuard: empty stream."
+newtype Apply2 α β γ = Apply2 { apply2 :: α -> β -> γ }
 
-evalStream :: Env -> Stream -> (Int, DynamicF [] Type)
-evalStream env (Stream t id buffer mguard e1) =
-    (id, toDynamicF ys t)
+instance Op2 Apply2 where
+  and      = Apply2 (&&)
+  or       = Apply2 (||)
+  add _    = Apply2 (+)
+  sub _    = Apply2 (-)
+  mul _    = Apply2 (*)
+  mod _    = Apply2 P.mod
+  div _    = Apply2 P.div
+  eq _     = Apply2 (==)
+  ne _     = Apply2 (/=)
+  le _     = Apply2 (<=)
+  ge _     = Apply2 (>=)
+  lt _     = Apply2 (<)
+  gt _     = Apply2 (>)
+
+--------------------------------------------------------------------------------
+
+newtype Apply3 α β γ δ = Apply3 { apply3 :: α -> β -> γ -> δ }
+
+instance Op3 Apply3 where
+  mux _    = Apply3 $ \ v x y -> if v then x else y
+
+--------------------------------------------------------------------------------
+
+type Output = (Bool, [String])
+
+--------------------------------------------------------------------------------
+
+evalStream :: Env Name -> Env Id -> Stream -> (Int, DynamicF [] Type)
+evalStream exts strms (Stream id buffer _ e1 t) = (id, toDynamicF xs t)
+
   where
-    xs = buffer ++ evalExpr e1 env
-    ys =
-      case mguard of
-        Just e2 -> withGuard (uninitialized t) (evalExpr e2 env) xs
-        Nothing -> xs
 
-evalTrigger :: Env -> Trigger -> [Output]
-evalTrigger env (Trigger _ _ guard value) =
-    zip bs (fmap show vs)
-  where
-    bs = evalExpr guard env
-    vs = evalExpr value env
+  xs = buffer ++ evalExpr exts strms e1
+--  ys =
+--    case mguard of
+--      Just e2 -> withGuard (uninitialized t) (evalExpr env e2) xs
+--      Nothing -> xs
 
-evalSpec :: Spec -> [[Output]]
-evalSpec spec = out
+--  withGuard :: α -> [Bool] -> [α] -> [α]
+--  withGuard _ (True:vs)  (x:xs) = x : withGuard x vs xs
+--  withGuard z (False:vs) xs     = z : withGuard z vs xs
+--  withGuard _ _          _      = error "withGuard: empty stream."
+
+--------------------------------------------------------------------------------
+
+evalTrigger :: Env Name -> Env Id -> Trigger -> [Output]
+evalTrigger exts strms (Trigger _ e args) = zip bs vs
+
   where
-    env = fmap (evalStream  env) (specStreams  spec)
-    out = fmap (evalTrigger env) (specTriggers spec)
+
+  bs :: [Bool]
+  bs = evalExpr exts strms e
+  
+  vs :: [[String]]
+  vs = map evalTriggerArg args
+
+  evalTriggerArg :: TriggerArg -> [String]
+  evalTriggerArg (TriggerArg e1 _) = map show (evalExpr exts strms e1)
+
+--------------------------------------------------------------------------------
+
+evalSpec :: Env Name -> Spec -> [[Output]]
+evalSpec exts spec = outps
+  where
+    strms = fmap (evalStream  exts strms) (specStreams  spec)
+    outps = fmap (evalTrigger exts strms) (specTriggers spec)
+
+--------------------------------------------------------------------------------
 
 -- | Interprets a Copilot specification.
-interpret :: Int -> Spec -> String
-interpret k spec =
+interpret :: Int -> Env Name -> Spec -> String
+interpret k exts spec =
   ( render
   . asColumns
   . transpose
   . (:) (ppTriggerNames $ specTriggers spec)
   . take k
   . ppOutputs
-  . evalSpec
+  . evalSpec exts
   ) spec
+
+--------------------------------------------------------------------------------
 
 -- Functions for pretty-printing the output of the interpreter:
 
@@ -134,9 +172,11 @@ ppTriggerNames :: [Trigger] -> [Doc]
 ppTriggerNames = map (text . (++ ":") . triggerName)
 
 ppOutput :: Output -> Doc
-ppOutput (True,  v) = text v
-ppOutput (False, _) = text "--"
+ppOutput (True,  vs) = text $ "(" ++ concat (intersperse "," vs) ++ ")"
+ppOutput (False, _ ) = text "--"
 
 ppOutputs :: [[Output]] -> [[Doc]]
 ppOutputs [] = []
 ppOutputs xs = map (ppOutput . head) xs : ppOutputs (map tail xs)
+
+--------------------------------------------------------------------------------
