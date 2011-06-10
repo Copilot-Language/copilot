cb54bb74a14ae133dc82bf321de01d26596725f0
Lee Pike <leepike@galois.com>
2011-06-10 14:56:10 -0700

    Voting example.

--- Examples/Examples.hs
+++ Examples/Examples.hs
@@ -7,6 +7,7 @@ import Copilot.Language
 import Copilot.Language.Prelude hiding (even)
 import Copilot.Language.Reify (reify)
 import Copilot.Compile.C99 (compile)
+import Copilot.Library.Voting 
 
 --
 -- Some utility functions:
@@ -39,6 +40,31 @@ sumExterns =
     e2 = extern "e2"
   in
     e1 + e2
+
+-- > interpret 10 [] vote 
+-- results in out of memory
+vote :: Copilot
+vote = do 
+  trigger "maj" true
+    [ arg maj ]
+  trigger "aMaj" true 
+    [ arg $ aMajority ls maj ]
+  where
+  ls  = [a, b, c, d, e] --, f, g, h, i, j, k, l, m]
+  maj = majority ls
+  a = [0] ++ a + 1 :: Stream Word32
+  b = [0] ++ b + 1
+  c = [0] ++ c + 1
+  d = [0] ++ d + 1
+  e = [1] ++ e + 1
+  -- f = [1] ++ f + 1
+  -- g = [1] ++ g + 1
+  -- h = [1] ++ h + 1
+  -- i = [1] ++ i + 1
+  -- j = [1] ++ j + 1
+  -- k = [1] ++ k + 1
+  -- l = [1] ++ l + 1
+  -- m = [1] ++ m + 1
 
 --
 -- An example of a specification:
