4bdb9ca68d7730bde637df7196f919870e38e528
Lee Pike <leepike@galois.com>
2011-06-10 16:43:50 -0700

    Forgotten type during merging.

--- Examples/Examples.hs
+++ Examples/Examples.hs
@@ -54,28 +54,28 @@ sumExterns =
 
 -- > interpret 10 [] vote 
 -- results in out of memory
-vote :: Copilot
+vote :: Spec
 vote = do 
   trigger "maj" true
     [ arg maj ]
   trigger "aMaj" true 
     [ arg $ aMajority ls maj ]
   where
-  ls  = [a, b, c, d, e] --, f, g, h, i, j, k, l, m]
+  ls  = [a, b, c, d, e, f, g, h, i, j, k, l, m]
   maj = majority ls
   a = [0] ++ a + 1 :: Stream Word32
   b = [0] ++ b + 1
   c = [0] ++ c + 1
   d = [0] ++ d + 1
   e = [1] ++ e + 1
-  -- f = [1] ++ f + 1
-  -- g = [1] ++ g + 1
-  -- h = [1] ++ h + 1
-  -- i = [1] ++ i + 1
-  -- j = [1] ++ j + 1
-  -- k = [1] ++ k + 1
-  -- l = [1] ++ l + 1
-  -- m = [1] ++ m + 1
+  f = [1] ++ f + 1
+  g = [1] ++ g + 1
+  h = [1] ++ h + 1
+  i = [1] ++ i + 1
+  j = [1] ++ j + 1
+  k = [1] ++ k + 1
+  l = [1] ++ l + 1
+  m = [1] ++ m + 1
 
 --------------------------------------------------------------------------------
 
