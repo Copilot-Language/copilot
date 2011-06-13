6385acefff25c1fb9c5a337b4d133c61fbed1e6b
Lee Pike <leepike@galois.com>
2011-06-13 16:49:09 -0700

    A little voting experiment.

--- Examples/Voting.hs
+++ Examples/Voting.hs
@@ -17,6 +17,32 @@ import Copilot.Library.Voting
 
 -- > interpret 10 [] vote 
 -- results in out of memory
+voteLet :: Spec
+voteLet = do 
+  let_ "v" (majority ls)
+  trigger "maj" true
+    [ arg (var "v" :: Stream Word32) ]
+  trigger "aMaj" true 
+    [ arg $ aMajority ls (var "v") ]
+  where
+  ls  = [a, b, c, d, e, f, g, h, i, j, k] --, l, m]
+  --maj = majority ls
+  a = [0] ++ a + 1 :: Stream Word32
+  b = [0] ++ b + 1
+  c = [0] ++ c + 1
+  d = [0] ++ d + 1
+  e = [1] ++ e + 1
+  f = [1] ++ f + 1
+  g = [1] ++ g + 1
+  h = [1] ++ h + 1
+  i = [1] ++ i + 1
+  j = [1] ++ j + 1
+  k = [1] ++ k + 1
+  -- l = [1] ++ l + 1
+  -- m = [1] ++ m + 1
+
+--------------------------------------------------------------------------------
+
 vote :: Spec
 vote = do 
   trigger "maj" true
@@ -24,7 +50,7 @@ vote = do
   trigger "aMaj" true 
     [ arg $ aMajority ls maj ]
   where
-  ls  = [a, b, c, d, e, f, g, h, i, j, k, l, m]
+  ls  = [a, b, c, d, e, f, g, h, i, j, k] --, l, m]
   maj = majority ls
   a = [0] ++ a + 1 :: Stream Word32
   b = [0] ++ b + 1
@@ -37,7 +63,13 @@ vote = do
   i = [1] ++ i + 1
   j = [1] ++ j + 1
   k = [1] ++ k + 1
-  l = [1] ++ l + 1
-  m = [1] ++ m + 1
+  --l = [1] ++ l + 1
+  -- m = [1] ++ m + 1
 
 --------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  putStrLn ""
+  interpret 3 [] vote
+  putStrLn ""
