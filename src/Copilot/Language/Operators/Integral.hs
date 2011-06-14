069201aeea3c75241bae70450704d2903d9a0521
Sebastian Niller <seni.eisfeld@googlemail.de>
2011-06-15 00:55:39 +0200

    case for mod

--- src/Copilot/Language/Operators/Integral.hs
+++ src/Copilot/Language/Operators/Integral.hs
@@ -24,6 +24,7 @@ x `div` y = Op2 (Core.div typeOf) x y
 
 mod :: (Typed a, P.Integral a) => Stream a -> Stream a -> Stream a
 (Const 0) `mod` _         = (Const 0)
+(Const 1) `mod` (Const 1) = (Const 0)
 (Const 1) `mod` _         = (Const 1)
 (Const x) `mod` (Const y) = Const (x `P.mod` y)
 x `mod` y = Op2 (Core.mod typeOf) x y
