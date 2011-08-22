70b9727de76068143ffa1fd6636fd4f231105e6e
Nis Wegmann <niswegmann@gmail.com>
2011-08-22 17:04:39 -0400

    Added parameters.

---
+++ src/Copilot/Compile/C99/Params.hs
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+-- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
+--------------------------------------------------------------------------------
+
+module Copilot.Compile.C99.Params (Params (..), defaultParams, withPrefix) where
+
+data Params = Params
+  { prefix :: Maybe String
+  }
+
+defaultParams :: Params
+defaultParams = Params
+  { prefix = Nothing
+  }
+
+withPrefix :: Maybe String -> String -> String
+withPrefix (Just cs) ds = cs ++ "_" ++ ds
+withPrefix _         ds = ds
