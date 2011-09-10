d58e4adc31d54f76d564c509647e919492f9c5ed
Lee Pike <leepike@galois.com>
2011-09-09 21:11:26 -0700

    New Copilot stuff.

--- Language/Copilot.hs
+++ Language/Copilot.hs
@@ -1,44 +1,39 @@
-module Language.Copilot
-  ( module Language.Copilot.Core 
-  , module Language.Copilot.Analyser
-  , module Language.Copilot.Interpreter
-  , module Language.Copilot.Help
-  , module Language.Copilot.AtomToC
-  , module Language.Copilot.Compiler
-  , module Language.Copilot.Language
-  -- , module Language.Copilot.Sampling
-  -- , module Language.Copilot.Casting
-  -- , module Language.Copilot.Dispatch
-  , module Language.Copilot.Interface
-  , module Language.Copilot.Tests.Random
-  -- , module Language.Copilot.Libs.Indexes
-  -- , module Language.Copilot.Libs.LTL
-  -- , module Language.Copilot.Libs.PTLTL
-  -- , module Language.Copilot.Libs.Clocks
-  -- , module Language.Copilot.AdHocC
-  -- , module Language.Copilot.Examples.Examples
-  -- , module Language.Copilot.Examples.LTLExamples
-  -- , module Language.Copilot.Examples.PTLTLExamples
+module Language.Copilot 
+  (
+    module Copilot.Language
+  , module Copilot.Language.Prelude 
+  , module Copilot.Language.Reify
+
+  -- Code generators
+  -- , module Copilot.Compile.C99
+  -- , module Copilot.Compile.SBV
+
+  -- Libraries
+  , module Copilot.Library.Clocks
+  , module Copilot.Library.LTL
+  , module Copilot.Library.PTLTL
+  , module Copilot.Library.Statistics
+  , module Copilot.Library.RegExp
+  , module Copilot.Library.Utils
+  , module Copilot.Library.Voting
+  , module Copilot.Library.Stacks
   ) where
 
-import Language.Copilot.Core (Streams, Spec, emptySM, StreamableMaps(..)) -- XXX for libs, probably need to export all the maps.  Maybe put in a different file?
-import Language.Copilot.Analyser
-import Language.Copilot.Interpreter
-import Language.Copilot.Help
-import Language.Copilot.AtomToC
-import Language.Copilot.Compiler
-import Language.Copilot.Tests.Random
-import Language.Copilot.Language
--- import Language.Copilot.Language.Sampling
--- import Language.Copilot.Language.Casting
--- import Language.Copilot.PrettyPrinter()
--- import Language.Copilot.Dispatch
-import Language.Copilot.Interface
--- import Language.Copilot.Libs.Indexes
--- import Language.Copilot.Libs.LTL
--- import Language.Copilot.Libs.PTLTL
--- import Language.Copilot.Libs.Clocks
--- import Language.Copilot.AdHocC
--- import Language.Copilot.Examples.Examples
--- import Language.Copilot.Examples.LTLExamples
--- import Language.Copilot.Examples.PTLTLExamples
+import Copilot.Language
+import Copilot.Language.Prelude 
+import Copilot.Language.Reify
+
+-- Code generators
+-- import Copilot.Compile.C99
+-- import Copilot.Compile.SBV
+
+-- Libraries
+import Copilot.Library.Clocks
+import Copilot.Library.LTL
+import Copilot.Library.PTLTL
+import Copilot.Library.Statistics
+import Copilot.Library.RegExp
+import Copilot.Library.Utils
+import Copilot.Library.Voting
+import Copilot.Library.Stacks
+
