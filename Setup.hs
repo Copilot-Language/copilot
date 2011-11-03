b649f427d6ac3d32528c2a38c421801bdaa56253
Lee Pike <leepike@galois.com>
2011-11-03 15:33:07 -0700

    Make an executable and give a notice after compiling.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,11 @@
 import Distribution.Simple
-main = defaultMain
+import System.Exit (exitWith, ExitCode(..))
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks{ postInst = msg }
+  where 
+  msg _ _ _ _ = do
+    putStrLn "Execute \"<Your install location>/.cabal/bin/copilot-regression\" to run some very simple tests to ensure your installation built correctly."
+    putStrLn "To QuickCheck the Atom backend, execute \"<Your install location>/.cabal/bin/copilot-c99-qc .\""
+    exitWith ExitSuccess
+
