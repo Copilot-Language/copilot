673afa7b2480a6bd67bfd42897993227f810a9a8
Lee Pike <leepike@galois.com>
2011-11-04 10:51:52 -0700

    Error module.

---
+++ src/Copilot/Language/Error.hs
@@ -0,0 +1,16 @@
+--------------------------------------------------------------------------------
+-- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
+--------------------------------------------------------------------------------
+
+module Copilot.Language.Error 
+  ( impossible
+  , badUsage ) where
+
+impossible :: String -> String -> a
+impossible function package = 
+  error $ "Impossible error in function " ++ function ++ ", in package " ++ 
+    package ++ ".  Please email Lee Pike at <lee pike @ gmail . com> " ++
+    "(remove spaces) or file a bug report on github.com."
+
+badUsage :: String -> a
+badUsage msg = error $ "Copilot error: " ++ msg ++ "."
