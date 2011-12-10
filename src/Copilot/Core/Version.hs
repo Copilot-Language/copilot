7b101efd572fdb27bb6ffbf56ad1cde4222365bb
Lee Pike <leepike@galois.com>
2011-12-10 14:02:14 -0800

    Remove version file.

--- src/Copilot/Core/Version.hs
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------------------------------
--- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
---------------------------------------------------------------------------------
-
-module Copilot.Core.Version (version) where
-
--- | The current version of Copilot Core.
-version :: String
-version = "0.1"
