9b8381bbbe2364e283609249dbd90600b3ccbe97
Lee Pike <leepike@galois.com>
2011-12-21 21:07:46 -0800

    Added simple types.

--- src/Copilot/Core/Type.hs
+++ src/Copilot/Core/Type.hs
@@ -10,6 +10,7 @@ module Copilot.Core.Type
   ( Type (..)
   , Typed (..)
   , UType (..)
+  , SimpleType(..)
   ) where
 
 import Data.Int
@@ -43,22 +44,66 @@ instance EqualType Type where
   (=~=) Double Double = Just Refl
   (=~=) _ _ = Nothing
 
+--------------------------------------------------------------------------------
+
+data SimpleType = SBool
+                | SInt8 
+                | SInt16
+                | SInt32
+                | SInt64
+                | SWord8
+                | SWord16
+                | SWord32
+                | SWord64
+                | SFloat 
+                | SDouble
+  deriving Eq
+
+--------------------------------------------------------------------------------
+
 class Typed a where
   typeOf :: Type a
+  simpleType :: Type a -> SimpleType
 
-instance Typed Bool   where typeOf = Bool
-instance Typed Int8   where typeOf = Int8
-instance Typed Int16  where typeOf = Int16
-instance Typed Int32  where typeOf = Int32
-instance Typed Int64  where typeOf = Int64
-instance Typed Word8  where typeOf = Word8
-instance Typed Word16 where typeOf = Word16
-instance Typed Word32 where typeOf = Word32
-instance Typed Word64 where typeOf = Word64
-instance Typed Float  where typeOf = Float
-instance Typed Double where typeOf = Double
+--------------------------------------------------------------------------------
+
+instance Typed Bool   where 
+  typeOf       = Bool
+  simpleType _ = SBool
+instance Typed Int8   where 
+  typeOf       = Int8
+  simpleType _ = SBool
+instance Typed Int16  where 
+  typeOf       = Int16
+  simpleType _ = SInt16
+instance Typed Int32  where 
+  typeOf       = Int32
+  simpleType _ = SInt32
+instance Typed Int64  where 
+  typeOf       = Int64
+  simpleType _ = SInt64
+instance Typed Word8  where 
+  typeOf       = Word8
+  simpleType _ = SWord8
+instance Typed Word16 where 
+  typeOf       = Word16
+  simpleType _ = SWord16
+instance Typed Word32 where 
+  typeOf       = Word32
+  simpleType _ = SWord32
+instance Typed Word64 where 
+  typeOf       = Word64
+  simpleType _ = SWord64
+instance Typed Float  where 
+  typeOf       = Float
+  simpleType _ = SFloat
+instance Typed Double where 
+  typeOf       = Double
+  simpleType _ = SDouble
 
 --------------------------------------------------------------------------------
 
 -- | A untyped type (no phantom type).
 data UType = forall a . UType { uTypeType :: Type a }
+
+--------------------------------------------------------------------------------
