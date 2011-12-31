8c68efd09487eb8667a457bb2e948bc7cf9e7373
Lee Pike <leepike@galois.com>
2011-12-30 22:19:51 -0800

    Make the compiler output files to a subdirectory.

--- src/Copilot/Compile/C99.hs
+++ src/Copilot/Compile/C99.hs
@@ -9,32 +9,38 @@ module Copilot.Compile.C99
   , module Copilot.Compile.C99.Params
   ) where
 
+import qualified Copilot.Core as Core
 import Copilot.Compile.Header.C99 (genC99Header)
 import Copilot.Compile.C99.MetaTable (allocMetaTable)
 import Copilot.Compile.C99.Params
 import Copilot.Compile.C99.Phases (schedulePhases)
 import Copilot.Compile.C99.PrePostCode (preCode, postCode)
-import qualified Copilot.Core as Core
+
+
 import Language.Atom (Atom)
 import qualified Language.Atom as Atom
+
 import Control.Monad (when)
+import System.Directory (createDirectory)
 
 --------------------------------------------------------------------------------
 
 compile :: Params -> Core.Spec -> IO ()
-compile params spec0 =
-  do
-    (schedule, _, _, _, _) <- Atom.compile programName atomDefaults atomProgram
-    when (verbose params) $ putStrLn (Atom.reportSchedule schedule)
-    genC99Header (prefix params) "." spec
+compile params spec0 = do
+  createDirectory dirName
+  (schedule, _, _, _, _) <- Atom.compile programName atomDefaults atomProgram
+  when (verbose params) $ putStrLn (Atom.reportSchedule schedule)
+  genC99Header (prefix params) dirName spec
 
   where
+
+  dirName = withPrefix (prefix params) "copilot-c99"
 
   spec :: Core.Spec
   spec = Core.makeTags spec0
 
   programName :: String
-  programName = withPrefix (prefix params) "copilot"
+  programName = dirName ++ "/" ++ withPrefix (prefix params) "copilot-c99"
 
   atomDefaults :: Atom.Config
   atomDefaults =
