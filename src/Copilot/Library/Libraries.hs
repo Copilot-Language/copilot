daf365dbe8d129c214cee806c42498ee0f392d9b
Lee Pike <leepike@gmail.com>
2012-02-27 11:57:51 -0800

    Package libs up into a single export module.

---
+++ src/Copilot/Library/Libraries.hs
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------
+-- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
+--------------------------------------------------------------------------------
+
+-- | Main import module for the front-end lanugage.
+
+module Copilot.Library.Libraries (
+    module Copilot.Library.Clocks
+  , module Copilot.Library.LTL
+  , module Copilot.Library.PTLTL
+  , module Copilot.Library.Statistics
+  , module Copilot.Library.RegExp
+  , module Copilot.Library.Utils
+  , module Copilot.Library.Voting
+  , module Copilot.Library.Stacks
+  ) where
+
+import Copilot.Library.Clocks
+import Copilot.Library.LTL
+import Copilot.Library.PTLTL
+import Copilot.Library.Statistics
+import Copilot.Library.RegExp
+import Copilot.Library.Utils
+import Copilot.Library.Voting
+import Copilot.Library.Stacks
+
