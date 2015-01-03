e9f4e287605d263f64baf9433e95e9d6c08caf04
leepike <leepike@gmail.com>
2015-01-02 20:53:54 -0800

    updates for ghc 7.8.3

--- src/Copilot/Core/External.hs
+++ src/Copilot/Core/External.hs
@@ -2,7 +2,7 @@
 -- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
--- {-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE Rank2Types #-}
 
