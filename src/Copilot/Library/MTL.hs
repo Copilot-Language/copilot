0b72fb440f66c427b2b7eae50e8280228603c18f
lmp47 <lmpick@zoho.com>
2015-08-19 23:14:45 -0400

    Smaller and simpler implementations

--- src/Copilot/Library/MTL.hs
+++ src/Copilot/Library/MTL.hs
@@ -18,52 +18,52 @@ import Copilot.Library.Utils
 -- where (t + l) <= t' <= (t + u)
 eventually :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool
-eventually l u clk dist s = res clk s ((u `P.div` dist) + 1)
+eventually l u clk dist s = res clk s $ (u `P.div` dist) + 1
   where
   mins = clk + (constant l)
   maxes = clk + (constant u)
   res _ _ 0 = false
   res c s k =
-    c <= maxes && ((mins <= c && s) || (nextRes c s k))
+    c <= maxes && ((mins <= c && s) || nextRes c s k)
   nextRes c s k = res (drop 1 c) (drop 1 s) (k - 1)
 
 -- EventuallyPrev: True at time t iff s is true at some time t',
 -- where (t - u) <= t' <= (t - l)
 eventuallyPrev :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool
-eventuallyPrev l u clk dist s = res clk s ((u `P.div` dist) + 1)
+eventuallyPrev l u clk dist s = res clk s $ (u `P.div` dist) + 1
   where
   mins = clk - (constant u)
   maxes = clk - (constant l)
   res _ _ 0 = false
   res c s k =
-    mins <= c && ((c <= maxes && s) || (nextRes c s k))
+    mins <= c && ((c <= maxes && s) || nextRes c s k)
   nextRes c s k = res ([0] ++ c) ([False] ++ s) (k - 1)
 
 -- Always: True at time t iff s is true at all times t'
 -- where (t + l) <= t' <= (t + u)
 always :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool
-always l u clk dist s = res clk s ((u `P.div` dist) + 1) 
+always l u clk dist s = res clk s $ (u `P.div` dist) + 1
   where 
   mins = clk + (constant l)
   maxes = clk + (constant u)
   res _ _ 0 = true 
   res c s k = 
-    c > maxes || ((mins <= c ==> s) && (nextRes c s k))
+    c > maxes || ((mins <= c ==> s) && nextRes c s k)
   nextRes c s k = res (drop 1 c) (drop 1 s) (k - 1)
 
 -- AlwaysBeen: True at time t iff s is true at all times t'
 -- where (t - u) <= t' <= (t - l)
 alwaysBeen :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool
-alwaysBeen l u clk dist s = res clk s ((u `P.div` dist) + 1)
+alwaysBeen l u clk dist s = res clk s $ (u `P.div` dist) + 1
   where
   mins = clk - (constant u)
   maxes = clk - (constant l)
   res _ _ 0 = true
   res c s k =
-    c < mins || ((c <= maxes ==> s) && (nextRes c s k))
+    c < mins || ((c <= maxes ==> s) && nextRes c s k)
   nextRes c s k = res ([0] ++ c) ([True] ++ s) (k - 1)
 
 -- Until: True at time t iff there exists a d with l <= d <= u
@@ -71,7 +71,7 @@ alwaysBeen l u clk dist s = res clk s ((u `P.div` dist) + 1)
 -- and for all times t' with t <= t' < t + d, s0 is true
 until :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool -> Stream Bool
-until l u clk dist s0 s1 = res clk s0 s1 ((u `P.div` dist) + 1)
+until l u clk dist s0 s1 = res clk s0 s1 $ (u `P.div` dist) + 1
   where
   mins = clk + (constant l)
   maxes = clk + (constant u)
@@ -85,13 +85,13 @@ until l u clk dist s0 s1 = res clk s0 s1 ((u `P.div` dist) + 1)
 -- and for all times t' with t - d < t' <= t, s0 is true
 since :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool -> Stream Bool
-since l u clk dist s0 s1 = res clk s0 s1 ((u `P.div` dist) + 1)
+since l u clk dist s0 s1 = res clk s0 s1 $ (u `P.div` dist) + 1
   where
   mins = clk - (constant u)
   maxes = clk - (constant l)
   res _ _ _ 0 = false 
   res c s s' k =
-    mins <= c && ((c <= maxes && s') || (s && (nextRes c s s' k)))
+    mins <= c && ((c <= maxes && s') || (s && nextRes c s s' k))
   nextRes c s s' k = res ([0] ++ c) ([True] ++ s) ([False] ++ s') (k - 1)
 
 -- Release: true at time t iff for all d with l <= d <= u where there
@@ -104,10 +104,10 @@ release l u clk dist s0 s1 = res' clk s1 $ (u `P.div` dist) + 1
   mins = clk + (constant l)
   maxes = clk + (constant u)
   res _ _ _ 0 = true
-  res c s s' k = 
-    s || ((mins > c || c > maxes || s') && (nextRes c s s' k))
-  res' c s' k =
-    (mins > c || c > maxes || s') && (res (drop 1 c) s0 (drop 1 s') (k - 1))
+  res c s s' k =
+    s || ((mins > c || c > maxes || s') && nextRes c s s' k)
+  res' c s' k = (mins > c || c > maxes || s') &&
+                (res (drop 1 c) s0 (drop 1 s') (k - 1))
   nextRes c s s' k = res (drop 1 c) (drop 1 s) (drop 1 s') (k - 1)
 
 -- Trigger: True at time t iff for all d with l <= d <= u where there
@@ -115,20 +115,16 @@ release l u clk dist s0 s1 = res' clk s1 $ (u `P.div` dist) + 1
 -- or s0 has a true sample at some time t' with t - d < t' <= t 
 trigger :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool -> Stream Bool
-trigger l u clk dist s0 s1 = res clk s1 iter
+trigger l u clk dist s0 s1 = res' clk s1 $ (u `P.div` dist) + 1
   where
   mins = clk - (constant u)
   maxes = clk - (constant l)
-  iter = (u `P.div` dist) + 1
-  res _ _ 0 = true
-  res c s k =
-    mux (mins > c || c > maxes || s)
-      (nextRes c s k)
-      (res' clk s0 (iter - k) c)
-  nextRes c s k = res ([0] ++ c) ([True] ++ s) (k - 1)
-  res' _ _ 0 _ = false
-  res' c s k lowl = (c > lowl && s) || (nextRes' c s k lowl)
-  nextRes' c s k lowl = res' ([0] ++ c) ([False] ++ s) (k - 1) lowl
+  res _ _ _ 0 = true
+  res c s s' k =
+    s || ((mins > c || c > maxes || s') && nextRes c s s' k)
+  res' c s' k = (mins > c || c > maxes || s') &&
+                (res ([0] ++ c) s0 ([True] ++ s') (k - 1))
+  nextRes c s s' k = res ([0] ++ c) ([False] ++ s) ([True] ++ s') (k - 1)
 
 -- Matching Variants
 
@@ -136,13 +132,13 @@ trigger l u clk dist s0 s1 = res clk s1 iter
 -- needing to hold at time (t + d) instead of just s1
 matchingUntil :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool -> Stream Bool
-matchingUntil l u clk dist s0 s1 = res clk s0 s1 ((u `P.div` dist) + 1)
+matchingUntil l u clk dist s0 s1 = res clk s0 s1 $ (u `P.div` dist) + 1
   where
   mins = clk + (constant l)
   maxes = clk + (constant u)
   res _ _ _ 0 = false
   res c s s' k =
-    (c <= maxes) && s && ((mins <= c && s') || (nextRes c s s' k))
+    c <= maxes && s && ((mins <= c && s') || nextRes c s s' k)
   nextRes c s s' k = res (drop 1 c) (drop 1 s) (drop 1 s') (k - 1)
 
 -- Matching Since: Same semantics as Since, except with both s1 and s0
@@ -160,12 +156,13 @@ matchingRelease l u clk dist s0 s1 = res clk s0 s1 $ (u `P.div` dist) + 1
   mins = clk + (constant l)
   maxes = clk + (constant u)
   res _ _ _ 0 = true
-  res c s s' k = 
-    s || ((mins > c || c > maxes || s') && (nextRes c s s' k))
+  res c s s' k =
+    s || ((mins > c || c > maxes || s') && nextRes c s s' k)
   nextRes c s s' k = res (drop 1 c) (drop 1 s) (drop 1 s') (k - 1)
 
 -- Matching Trigger: Same semantics as Trigger, except with
 -- s1 or s0 needing to hold at time (t - d) instead of just s1
 matchingTrigger :: ( Typed a, Integral a ) =>
   a -> a -> Stream a -> a -> Stream Bool -> Stream Bool -> Stream Bool
-matchingTrigger l u clk dist s0 s1 = Copilot.Library.MTL.trigger l u clk dist s0 (s0 || s1)
+matchingTrigger l u clk dist s0 s1 =
+  Copilot.Library.MTL.trigger l u clk dist s0 (s0 || s1)
