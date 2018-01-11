8c9c49f2964884304c2150fa29cdf402c9dc1f60
Frank Dedden <dev@dedden.net>
2018-01-11 15:48:36 -0500

    Added 'normalize' to exports as well

--- src/Copilot/Backend/C.hs
+++ src/Copilot/Backend/C.hs
@@ -1,3 +1,4 @@
-module Copilot.Backend.C ( compile, testcompile ) where
+module Copilot.Backend.C ( compile, testcompile, normalize ) where
 
 import Copilot.Backend.C.CodeGen
+import Copilot.Backend.C.Normalize
