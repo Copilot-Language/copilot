8ca56d8546d6e575f0457ef4a6298e26522e8966
Frank Dedden <dev@dedden.net>
2019-02-13 11:58:52 +0100

    Removed most of the old implementation for a new start

--- src/Copilot/Compile/C.hs
+++ src/Copilot/Compile/C.hs
@@ -1,10 +1,1 @@
-module Copilot.Compile.C
-  ( compile
-  , normalize
-  ) where
-
-import Copilot.Compile.C.Normalize
-import Copilot.Compile.C.Compile
-
-import Copilot.Compile.ACSL.Expr
-import Copilot.Compile.ACSL.CodeGen
+module Copilot.Compile.C where
