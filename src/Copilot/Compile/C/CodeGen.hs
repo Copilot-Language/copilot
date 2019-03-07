f6ccf51c27eb586155fce5bf998c8ea2e1df5fb8
Frank Dedden <dev@dedden.net>
2019-03-07 15:40:51 +0100

    Moved mk*decln and mkinits to CodeGen, removed mkvaldecln, transspec and transstream

---
+++ src/Copilot/Compile/C/CodeGen.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE GADTs #-}
+
+module Copilot.Compile.C.CodeGen where
+
+import qualified Language.C99.Simple as C
+
+import Copilot.Core
+import Copilot.Compile.C.Util
+import Copilot.Compile.C.Translate
+
+
+-- | Make a C buffer variable and initialise it with the stream buffer.
+mkbuffdecln :: Id -> Type a -> [a] -> C.Decln
+mkbuffdecln sid ty xs = C.Decln (Just C.Static) cty name initvals where
+  name     = buffername sid
+  cty      = C.Array (transtype ty) (Just $ C.LitInt $ fromIntegral buffsize)
+  buffsize = length xs
+  initvals = mkinits ty xs
+
+-- | Make a C index variable and initialise it to 0.
+mkindexdecln :: Id -> C.Decln
+mkindexdecln sid = C.Decln (Just C.Static) cty name initval where
+  name    = indexname sid
+  cty     = C.TypeSpec $ C.TypedefName "size_t"
+  initval = C.InitExpr $ C.LitInt 0
+
+-- | Make an initial declaration from a list of values.
+mkinits :: Type a -> [a] -> C.Init
+mkinits ty xs = C.InitArray $ map (mkinit ty) xs
+
+-- | Make an initial declaration from a single value.
+mkinit :: Type a -> a -> C.Init
+mkinit (Array ty') xs = C.InitArray $ map (mkinit ty') (arrayelems xs)
+mkinit ty          x  = C.InitExpr  $ constty ty x
