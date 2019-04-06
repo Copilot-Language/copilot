acb2a9cd4700418eaa3d245325d8cd8d56eeab95
Frank Dedden <dev@dedden.net>
2019-04-06 18:19:16 +0200

    Fixed bugs and typos in Heater.hs. Refs #28

--- Examples/Heater.hs
+++ Examples/Heater.hs
@@ -1,10 +1,10 @@
 --------------------------------------------------------------------------------
--- Copyright © 2019 National Institute of Aerospace / Galois, Inc.
+-- Copyright 2019 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
 -- This is a simple example with basic usage. It implements a simple home
 -- heating system: It heats when temp gets too low, and stops when it is high
--- enough. It read temperature as an byte (range -50C to 100C) and translates
+-- enough. It read temperature as a byte (range -50C to 100C) and translates
 -- this to Celcius.
 
 module Heater where
@@ -15,20 +15,20 @@ import Copilot.Compile.C99
 import Prelude hiding ((>), (<), div)
 
 -- External temperature as a byte, range of -50C to 100C
-temp :: Stream Int8
+temp :: Stream Word8
 temp = extern "temperature" Nothing
 
 -- Calculate temperature in Celcius.
--- We need to cast the Int8 to a Float. Note that it is an unsafeCast, as there
--- is no direct relation between Int8 and Float.
+-- We need to cast the Word8 to a Float. Note that it is an unsafeCast, as there
+-- is no direct relation between Word8 and Float.
 ctemp :: Stream Float
-ctemp = ((unsafeCast temp) / 150.0) - 50.0
+ctemp = (unsafeCast temp) * (150.0 / 255.0) - 50.0
 
 spec = do
-  -- Triggers that fire when the ctemp is too low or too hight,
+  -- Triggers that fire when the ctemp is too low or too high,
   -- pass the current ctemp as an argument.
   trigger "heaton"  (ctemp < 18.0) [arg ctemp]
-  trigger "heafoff" (ctemp > 21.0) [arg ctemp]
+  trigger "heatoff" (ctemp > 21.0) [arg ctemp]
 
 -- Compile the spec
 main = reify spec >>= compile "heater"
