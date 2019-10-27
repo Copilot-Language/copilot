a3e0b59afb31af1e47acf2beabae1a2c28ed3241
Frank Dedden <dev@dedden.net>
2019-10-27 20:23:42 +0100

    Updated AddMult example and added copilot.cabal entrya. Refs #41

--- Examples/AddMult.hs
+++ Examples/AddMult.hs
@@ -2,35 +2,20 @@
 -- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
--- | Another small example.
+-- | Another small example that calculates a constant value using a recursive
+-- function.
 
-module AddMult ( addMult ) where
+module Main where
 
-import Prelude ()
 import Language.Copilot
-
---------------------------------------------------------------------------------
+import Copilot.Compile.C99
 
 spec :: Spec
-spec = 
-  trigger "f" true [ arg $ mult 5 ]
-
+spec = trigger "f" true [ arg $ mult 5 ]
   where
-  mult :: Word64 -> Stream Word64
-  mult 0 = 1
-  mult i = constant i * mult (i-1)
+    mult :: Word64 -> Stream Word64
+    mult 0 = 1
+    mult i = constant i * mult (i-1)
 
-addMult :: IO ()
-addMult = do
-  putStrLn "PrettyPrinter:"
-  putStrLn ""
-  prettyPrint spec
-  putStrLn ""
-  putStrLn ""
-  putStrLn "Interpreter:"
-  putStrLn ""
-  interpret 100 spec
-
---------------------------------------------------------------------------------
-
-main = addMult
+main :: IO ()
+main = interpret 100 spec
