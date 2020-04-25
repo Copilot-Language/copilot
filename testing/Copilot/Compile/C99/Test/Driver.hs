4a10c9bc1b5299da45863c5584aa1a2879a86249
Frank Dedden <dev@dedden.net>
2020-04-25 15:27:40 +0200

    Implemented mktrigger and framework for mkprintfcsv. Refs #185.

--- testing/Copilot/Compile/C99/Test/Driver.hs
+++ testing/Copilot/Compile/C99/Test/Driver.hs
@@ -2,15 +2,36 @@ module Copilot.Compile.C99.Test.Driver where
 
 import Language.C99.Simple
 
-import Copilot.Core       as Core     (Spec (..))
+import Copilot.Core       as Core
+  ( Spec    (..)
+  , UExpr   (..)
+  , Trigger (..)
+  )
+import Copilot.Compile.C99.Translate  (transtype)
+import Copilot.Compile.C99.Util       (argnames)
 
 
 -- | Write a C driver mimicking Copilot's interpreter.
 mkdriver :: Spec -> TransUnit
 mkdriver spec = TransUnit vardefs fundefs
   where
-    vardefs = []
-    fundefs = [ mkmain 30 spec ]
+    vardefs   = []
+    fundefs   = ctriggers ++ [ mkmain 30 spec ]
+    ctriggers = map mktrigger (specTriggers spec)
+
+
+-- | Write a trigger function which prints all its arguments in a CSV like
+-- format.
+mktrigger :: Trigger -> FunDef
+mktrigger (Trigger name guard args) = FunDef returntype name params [] body
+  where
+    returntype = TypeSpec Void
+    namedargs  = zip (argnames name) args
+    params     = map mkparam namedargs
+    body       = [Expr $ mkprintfcsv namedargs]
+
+    mkparam :: (String, UExpr) -> Param
+    mkparam (name, UExpr ty _) = Param (transtype ty) name
 
 
 -- | Write the main function. The purpose of this function is to call the
@@ -27,3 +48,8 @@ mkmain iters spec = FunDef (TypeSpec Int) "main" params decln body
                   (UnaryOp Inc (Ident "i"))
                   [Expr $ Funcall (Ident "step") []]
              ]
+
+
+-- | Write a call to printf with a format resembling CSV.
+mkprintfcsv :: [(String, UExpr)] -> Expr
+mkprintfcsv = error "mkprintfcsv not implemented yet."
