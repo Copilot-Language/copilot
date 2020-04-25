69e7f91acd2cbbf4b8f00dd8a1a018b0a233017e
Frank Dedden <dev@dedden.net>
2020-04-25 12:35:51 +0200

    Implemented basic main function for driver. Refs #185.

---
+++ testing/Copilot/Compile/C99/Test/Driver.hs
@@ -0,0 +1,20 @@
+module Copilot.Compile.C99.Test.Driver where
+
+import Language.C99.Simple
+
+import Copilot.Core       as Core     (Spec (..))
+
+-- | Write the main function. The purpose of this function is to call the
+-- step-function a number of times.
+mkmain :: Int -> Spec -> FunDef
+mkmain iters spec = FunDef (TypeSpec Int) "main" params decln body
+  where
+    params = [ Param (TypeSpec Int) "argv"
+             , Param (Const $ Array (Ptr $ TypeSpec Char) Nothing) "argc"
+             ]
+    decln  = [VarDecln Nothing (TypeSpec Int) "i" (Just $ InitExpr $ LitInt 0)]
+    body   = [For (Ident "i" .= LitInt 0)
+                  (Ident "i" .< LitInt (fromIntegral iters))
+                  (UnaryOp Inc (Ident "i"))
+                  [Expr $ Funcall (Ident "step") []]
+             ]
