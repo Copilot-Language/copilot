9f596ed2269744e2e72798dd3a66ce0debf84111
Frank Dedden <dev@dedden.net>
2020-05-14 11:55:39 +0200

    Implemented prop_matching_output. Refs #185.
    This property specifies the equality between the output of the C99 code (with
    the driver) and the interpreter.

--- testing/Copilot/Compile/C99/Test/Property.hs
+++ testing/Copilot/Compile/C99/Test/Property.hs
@@ -1,8 +1,19 @@
 module Copilot.Compile.C99.Test.Property where
 
+import Data.Either                      (isRight)
 import System.Process                   (readProcess)
 
+import Text.CSV
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+
+-- We import Copilot.Language vs Language.Copilot* as that would create a
+-- dependency cycle. It forces us to import from multiple files however.
 import Copilot.Core                     (Spec)
+import Copilot.Core.Interpret           (interpret, Format(..))
+import qualified Copilot.Language as L  (Spec)
+import Copilot.Language.Reify           (reify)
+import Copilot.Language.Arbitrary
 import Copilot.Compile.C99              (compile)
 
 import Copilot.Compile.C99.Test.Driver
@@ -31,3 +42,28 @@ compiletest specname mainfile = do
 -- | Run the compiled specification and driver.
 runtest :: String -> IO String
 runtest specname = readProcess ("./" ++ specname) [] ""
+
+
+-- | For a given specification, the output of the driver should match the
+-- output of Copilots interpreter.
+prop_matching_output :: L.Spec -> Property
+prop_matching_output langspec = monadicIO $ do
+  let specname = "testspec"
+      mainfile = specname ++ "_main.c"
+      iters    = 30
+  spec <- run $ reify langspec
+  run $ writetest   specname mainfile spec iters
+  run $ compiletest specname mainfile
+
+  -- Run the interpreter and parse CSV.
+  let interpout    = interpret CSV iters spec
+      interpcsv    = parseCSV "" interpout
+      interpparsed = counterexample "Interpreter output parse failed."
+                      (isRight interpcsv)
+
+  -- Run the test and parse CSV.
+  c99out <- run $ runtest specname
+  let c99csv    = parseCSV "" c99out
+      c99parsed = counterexample "C99 output parse failed." (isRight c99csv)
+
+  return $ c99parsed .&&. interpparsed .&&. c99csv === interpcsv
