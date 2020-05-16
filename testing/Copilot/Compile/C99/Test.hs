7688b7afc833a7e72047d305a47fedbe9e745663
Frank Dedden <dev@dedden.net>
2020-05-16 12:45:23 +0200

    Moved write/compile/runtest to Test.hs. Refs #180.
    Moving these functions to their own file is a necessity when using a single
    file per test/property. We have to be able to reuse those functions. The
    imports in the originating file has also been minimised.

---
+++ testing/Copilot/Compile/C99/Test.hs
@@ -0,0 +1,32 @@
+module Copilot.Compile.C99.Test where
+
+import System.Process                   (readProcess)
+
+import Copilot.Core                     (Spec)
+import Copilot.Compile.C99              (compile)
+
+import Copilot.Compile.C99.Driver
+
+
+-- | Compile the specification and generate the test driver, then write these
+-- to specname.[ch] and a main file.
+writetest :: String -> String -> Spec -> Int -> IO ()
+writetest specname mainfile spec iters = do
+  let drivercode = writedriver specname spec iters
+  writeFile mainfile drivercode
+  compile specname spec
+
+
+-- | Compile the C code to using GCC to a binary.
+-- This function fails if the files names specname.c and mainfile do not exist.
+compiletest :: String -> String -> [String] -> IO String
+compiletest specname mainfile cflags = do
+  let output = ["-o", specname]
+      cfiles = [mainfile, specname ++ ".c"]
+      args   = cflags ++ output ++ cfiles
+  readProcess "gcc" args ""
+
+
+-- | Run the compiled specification and driver.
+runtest :: String -> IO String
+runtest specname = readProcess ("./" ++ specname) [] ""
