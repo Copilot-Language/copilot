496cb16bd037aa6bb58251504e037890b3bc098d
Frank Dedden <dev@dedden.net>
2021-01-25 19:44:13 +0100

    Added comments to the WCV example. Refs #63.

--- Examples/WCV.hs
+++ Examples/WCV.hs
@@ -1,3 +1,8 @@
+-- | This example shows an implementation of the Well-Clear Violation
+-- algorithm, it follows the implementation described in 'Analysis of
+-- Well-Clear Bounday Models for the Integration of UAS in the NAS',
+-- https://ntrs.nasa.gov/citations/20140010078.
+
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RebindableSyntax #-}
 
@@ -11,58 +16,97 @@ import Data.Foldable (forM_)
 import Data.Parameterized.Some
 import qualified Control.Monad as Monad
 
-dthr, tthr, zthr, tcoathr :: Stream Double
-dthr    = extern "dthr" Nothing
-tthr    = extern "tthr" Nothing
-zthr    = extern "zthr" Nothing
+
+-- | `dthr` is the horizontal distance threshold.
+dthr :: Stream Double
+dthr = extern "dthr" Nothing
+
+-- | `tthr` is the horizontal time threshold.
+tthr :: Stream Double
+tthr = extern "tthr" Nothing
+
+-- | `zthr` is the vertical distance / altitude threshold.
+zthr :: Stream Double
+zthr = extern "zthr" Nothing
+
+-- | `tcoathr` is the vertical time threshold.
+tcoathr :: Stream Double
 tcoathr = extern "tcoathr" Nothing
 
 type Vect2 = (Stream Double, Stream Double)
 
+
 --------------------------------
--- Relative velocity/position --
+-- External streams for relative position and velocity.
 --------------------------------
 
-vx, vy, vz :: Stream Double
+-- | The relative x velocity between ownship and the intruder.
+vx :: Stream Double
 vx = extern "relative_velocity_x" Nothing
+
+-- | The relative y velocity between ownship and the intruder.
+vy :: Stream Double
 vy = extern "relative_velocity_y" Nothing
+
+-- | The relative z velocity between ownship and the intruder.
+vz :: Stream Double
 vz = extern "relative_velocity_z" Nothing
 
+-- | The relative velocity as a 2D vector.
 v :: (Stream Double, Stream Double)
 v = (vx, vy)
 
-sx, sy, sz :: Stream Double
+
+-- | The relative x position between ownship and the intruder.
+sx :: Stream Double
 sx = extern "relative_position_x" Nothing
+
+-- | The relative y position between ownship and the intruder.
+sy :: Stream Double
 sy = extern "relative_position_y" Nothing
+
+-- | The relative z position between ownship and the intruder.
+sz :: Stream Double
 sz = extern "relative_position_z" Nothing
 
+-- | The relative position as a 2D vector.
 s :: (Stream Double, Stream Double)
 s = (sx, sy)
 
+
 ------------------
--- Vector stuff --
+-- The following section contains basic libraries for working with vectors.
 ------------------
 
+-- | Multiply two Vectors.
 (|*|) :: Vect2 -> Vect2 -> Stream Double
 (|*|) (x1, y1) (x2, y2) = (x1 * x2) + (y1 * y2)
 
+-- | Calculate the square of a vector.
 sq :: Vect2 -> Stream Double
 sq x = x |*| x
 
+-- | Calculate the length of a vector.
 norm :: Vect2 -> Stream Double
 norm = sqrt . sq
 
+-- | Calculate the determinant of two vectors.
 det :: Vect2 -> Vect2 -> Stream Double
 det (x1, y1) (x2, y2) = (x1 * y2) - (x2 * y1)
 
+-- | Compare two vectors, taking into account the small error that is
+-- introduced by the usage of `Double`s.
 (~=) :: Stream Double -> Stream Double -> Stream Bool
 a ~= b = (abs (a - b)) < 0.001
 
+-- | Negate a vector.
 neg :: Vect2 -> Vect2
 neg (x, y) = (negate x, negate y)
 
+
 --------------------
--- Time variables --
+-- From here on the algorithm, as described by the paper mentioned on the top
+-- of this file, is implemented. Please refer to the paper for details.
 --------------------
 
 tau :: Vect2 -> Vect2 -> Stream Double
@@ -92,9 +136,6 @@ delta s v d = (d*d) * (sq v) - ((det s v)*(det s v))
 theta :: Vect2 -> Vect2 -> Stream Double -> Stream Double -> Stream Double
 theta s v d e = (-(s |*| v) + e * (sqrt $ delta s v d)) / (sq v)
 
---------------------------
--- Some tools for times --
---------------------------
 
 tcoa :: Stream Double -> Stream Double -> Stream Double
 tcoa sz vz = if (sz * vz) < 0
@@ -104,10 +145,12 @@ tcoa sz vz = if (sz * vz) < 0
 dcpa :: Vect2 -> Vect2 -> Stream Double
 dcpa s@(sx, sy) v@(vx, vy) = norm (sx + (tcpa s v) * vx, sy + (tcpa s v) * vy)
 
+
 --------------------------
 -- Well clear Violation --
 --------------------------
 
+-- | Determines if the well clear property is violated or not.
 wcv :: (Vect2 -> Vect2 -> Stream Double) ->
        Vect2 -> Stream Double ->
        Vect2 -> Stream Double ->
@@ -127,6 +170,7 @@ horizontalWCV tvar s v =
 spec = do
   Monad.void $ prop "1a" (forall $ (tau s v) ~= (tau (neg s) (neg v)))
   -- Monad.void $ prop "3d" (forall $ (wcv tep s sz v vz)    == (wcv tep (neg s) (-sz) (neg v) (-vz)))
+
 
 main :: IO ()
 main = do
