bace923232ebe6d35c8c782140e22c695e448f59
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-01-27 14:26:50 -0500

    Document two modules. Refs #112.
    Document the modules Copilot and Copilot.Language.

--- src/Copilot.hs
+++ src/Copilot.hs
@@ -2,7 +2,10 @@
 -- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
--- | Main import module for the front-end lanugage.
+-- | Main import module for the front-end language.
+--
+-- This is a facility so that users of Copilot only need to @import Copilot@ in
+-- their modules, together with the specific backend they intend to use.
 
 {-# LANGUAGE Safe #-}
 
