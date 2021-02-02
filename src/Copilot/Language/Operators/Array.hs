2286d0eba7e1cfafcdb90fb7c3ee83b790ab1302
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-02 08:26:08 -0500

    Document Copilot.Language.Operators.Array module. Refs #112.

--- src/Copilot/Language/Operators/Array.hs
+++ src/Copilot/Language/Operators/Array.hs
@@ -2,6 +2,7 @@
 
 {-# LANGUAGE TypeFamilies #-}
 
+-- | Combinators to deal with streams carrying arrays.
 module Copilot.Language.Operators.Array
   ( (.!!)
   ) where
@@ -19,6 +20,12 @@ import GHC.TypeLits             (KnownNat)
 
 --------------------------------------------------------------------------------
 
+-- | Create a stream that carries an element of an array in another stream.
+--
+-- This function implements a projection of the element of an array at a given
+-- position, over time. For example, if @s@ is a stream of type @Stream (Array
+-- '5 Word8)@, then @s .!! 3@ has type @Stream Word8@ and contains the 3rd
+-- element (starting from zero) of the arrays in @s@ at any point in time.
 (.!!) :: ( KnownNat n
          , t' ~ InnerType t
          , Flatten t t'
