be73816e97213629d3b23c7f2604228f8b84e3fb
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-02 08:41:00 -0500

    Document Copilot.Language.Operators.Constant module. Refs #112.

--- src/Copilot/Language/Operators/Constant.hs
+++ src/Copilot/Language/Operators/Constant.hs
@@ -2,10 +2,10 @@
 -- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
--- | Constants.
-
 {-# LANGUAGE Safe #-}
 
+
+-- | Primitives to build constant streams.
 module Copilot.Language.Operators.Constant
   ( constant
   , constB
@@ -29,30 +29,64 @@ import Data.Int
 
 --------------------------------------------------------------------------------
 
+-- | Create a constant stream that is equal to the given argument, at any
+-- point in time.
 constant :: Typed a => a -> Stream a
 constant = Const
 
 --------------------------------------------------------------------------------
 
-constB   :: Bool -> Stream Bool
-constB   = constant
-constW8  :: Word8 -> Stream Word8
-constW8  = constant
+-- | Create a constant stream carrying values of type 'Bool' that is equal to
+-- the given argument, at any point in time.
+constB :: Bool -> Stream Bool
+constB = constant
+
+-- | Create a constant stream carrying values of type 'Word8' that is equal to
+-- the given argument, at any point in time.
+constW8 :: Word8 -> Stream Word8
+constW8 = constant
+
+-- | Create a constant stream carrying values of type 'Word16' that is equal to
+-- the given argument, at any point in time.
 constW16 :: Word16 -> Stream Word16
 constW16 = constant
+
+-- | Create a constant stream carrying values of type 'Word32' that is equal to
+-- the given argument, at any point in time.
 constW32 :: Word32 -> Stream Word32
 constW32 = constant
+
+-- | Create a constant stream carrying values of type 'Word64' that is equal to
+-- the given argument, at any point in time.
 constW64 :: Word64 -> Stream Word64
 constW64 = constant
-constI8  :: Int8 -> Stream Int8
-constI8  = constant
+
+-- | Create a constant stream carrying values of type 'Int8' that is equal to
+-- the given argument, at any point in time.
+constI8 :: Int8 -> Stream Int8
+constI8 = constant
+
+-- | Create a constant stream carrying values of type 'Int16' that is equal to
+-- the given argument, at any point in time.
 constI16 :: Int16 -> Stream Int16
 constI16 = constant
+
+-- | Create a constant stream carrying values of type 'Int32' that is equal to
+-- the given argument, at any point in time.
 constI32 :: Int32 -> Stream Int32
 constI32 = constant
+
+-- | Create a constant stream carrying values of type 'Int64' that is equal to
+-- the given argument, at any point in time.
 constI64 :: Int64 -> Stream Int64
 constI64 = constant
-constF   :: Float -> Stream Float
-constF   = constant
-constD   :: Double -> Stream Double
-constD   = constant
+
+-- | Create a constant stream carrying values of type 'Float' that is equal to
+-- the given argument, at any point in time.
+constF :: Float -> Stream Float
+constF = constant
+
+-- | Create a constant stream carrying values of type 'Double' that is equal to
+-- the given argument, at any point in time.
+constD :: Double -> Stream Double
+constD = constant
