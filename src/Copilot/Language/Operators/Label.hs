9b525eb08b9bdd1b1c061aa2ea8fe0440109d6cd
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-05 08:34:19 -0500

    Document module Copilot.Language.Operators.Label. Refs #112.

--- src/Copilot/Language/Operators/Label.hs
+++ src/Copilot/Language/Operators/Label.hs
@@ -2,7 +2,7 @@
 -- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
--- | Let expressions.
+-- | Label a stream with additional information.
 
 {-# LANGUAGE Safe #-}
 
@@ -15,6 +15,13 @@ import Copilot.Language.Stream (Stream (..))
 
 --------------------------------------------------------------------------------
 
+-- | This function allows you to label a stream with a tag, which can be used
+-- by different backends to provide additional information either in error
+-- messages or in the generated code (e.g., for traceability purposes).
+--
+-- Semantically, a labelled stream is just the stream inside it. The use of
+-- label should not affect the observable behavior of the monitor, and how it
+-- is used in the code generated is a decision specific to each backend.
 label :: (Typed a) => String -> Stream a -> Stream a
 label = Label
 
