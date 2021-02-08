08619b0df3b21913eef74f82230f446c050f48c8
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-08 13:20:30 -0500

    Improve documentation of module Copilot.Core.Type.Dynamic. Refs #145.

--- src/Copilot/Core/Type/Dynamic.hs
+++ src/Copilot/Core/Type/Dynamic.hs
@@ -28,24 +28,32 @@ import Copilot.Core.Type.Equality
 
 --------------------------------------------------------------------------------
 
+-- | Representation of a value accompanied by its type.
 data Dynamic :: (* -> *) -> * where
   Dynamic :: a -> t a -> Dynamic t
 
+-- | Representation of a function accompanied by its type.
 data DynamicF :: (* -> *) -> (* -> *) -> * where
   DynamicF :: f a -> t a -> DynamicF f t
 
+-- | Enclose a value and its type in a container.
 toDyn :: t a -> a -> Dynamic t
 toDyn t x = Dynamic x t
 
+-- | Extract a value from a dynamic. Return 'Nothing' if the value is not of
+-- the given type.
 fromDyn :: EqualType t => t a -> Dynamic t -> Maybe a
 fromDyn t1 (Dynamic x t2) =
   case t1 =~= t2 of
     Just Refl -> return x
     Nothing   -> Nothing
 
+-- | Enclose a function and its type in a container.
 toDynF :: t a -> f a -> DynamicF f t
 toDynF t fx = DynamicF fx t
 
+-- | Extract a value from a dynamic function container. Return 'Nothing' if
+-- the value is not of the given type.
 fromDynF :: EqualType t => t a -> DynamicF f t -> Maybe (f a)
 fromDynF t1 (DynamicF fx t2) =
   case t1 =~= t2 of
