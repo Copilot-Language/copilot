8818d6e1d41575a91b9adcea47fdf2b7d5bfc49d
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-08 13:06:06 -0500

    Improve documentation of module Copilot.Core.Type.Equality. Refs #145.

--- src/Copilot/Core/Type/Equality.hs
+++ src/Copilot/Core/Type/Equality.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE Safe #-}
 {-# LANGUAGE GADTs, KindSignatures #-}
 
+-- | Propositional equality and type equality.
 module Copilot.Core.Type.Equality
   ( Equal (..)
   , EqualType (..)
@@ -15,23 +16,31 @@ module Copilot.Core.Type.Equality
   , cong
   ) where
 
+-- | Propositional equality.
 data Equal :: * -> * -> * where
   Refl :: Equal a a
 
+-- | Type equality. If the value of @x =~= y@ is @Just Refl@, then the two
+-- types @x@ and @y@ are equal.
 class EqualType t where
   (=~=) :: t a -> t b -> Maybe (Equal a b)
 
+-- | Safe coercion, which requires proof of equality.
 coerce :: Equal a b -> a -> b
 coerce Refl x = x
 
+-- | Proof of propositional equality.
 refl :: Equal a a
 refl = Refl
 
+-- | Symmetry.
 symm :: Equal a b -> Equal b a
 symm Refl = Refl
 
+-- | Transitivity.
 trans :: Equal a b -> Equal b c -> Equal a c
 trans Refl Refl = Refl
 
+-- | Congruence.
 cong :: Equal a b -> Equal (f a) (f b)
 cong Refl = Refl
