72d9b6af1f51bfbf867c87bc87f8ca55021cc944
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-10 14:45:01 -0500

    Provide part of the documentation for Copilot.Theorem.Kind2.AST. Refs #95.

--- src/Copilot/Theorem/Kind2/AST.hs
+++ src/Copilot/Theorem/Kind2/AST.hs
@@ -2,14 +2,17 @@
 
 {-# LANGUAGE Safe #-}
 
+-- | Abstract syntax tree of Kind2 files.
 module Copilot.Theorem.Kind2.AST where
 
 --------------------------------------------------------------------------------
 
+-- | A file is a sequence of predicates and propositions.
 data File = File
   { filePreds     :: [PredDef]
   , fileProps     :: [Prop] }
 
+-- | A proposition is defined by a term.
 data Prop = Prop
   { propName      :: String
   , propTerm      :: Term }
@@ -25,6 +28,10 @@ data StateVarDef = StateVarDef
   , varType       :: Type
   , varFlags      :: [StateVarFlag] }
 
+-- | Types used in Kind2 files to represent Copilot types.
+--
+-- The Kind2 backend provides functions to, additionally, constrain the range
+-- of numeric values depending on their Copilot type ('Int8', 'Int16', etc.).
 data Type = Int | Real | Bool
 
 data StateVarFlag = FConst
