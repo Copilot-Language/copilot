6224c93faa91e5a4225f69583deaaf40ae20f313
Frank Dedden <dev@dedden.net>
2021-02-16 20:09:40 +0100

    Removed documentation for Copilot module. Refs #112.
    The file is likely to be removed, see issue #107.

--- src/Copilot.hs
+++ src/Copilot.hs
@@ -2,11 +2,6 @@
 -- Copyright © 2011 National Institute of Aerospace / Galois, Inc.
 --------------------------------------------------------------------------------
 
--- | Main import module for the front-end language.
---
--- This is a facility so that users of Copilot only need to @import Copilot@ in
--- their modules, together with the specific backend they intend to use.
-
 {-# LANGUAGE Safe #-}
 
 module Copilot
