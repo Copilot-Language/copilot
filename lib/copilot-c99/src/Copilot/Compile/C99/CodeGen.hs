0034b1c66fffdb489bcd6e4e627d07a06b4540c0
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-08-16 10:55:36 -0400

    Format code to max 80 lines. Refs #238.

--- lib/copilot-c99/src/Copilot/Compile/C99/CodeGen.hs
+++ lib/copilot-c99/src/Copilot/Compile/C99/CodeGen.hs
@@ -58,13 +58,14 @@ mkindexdecln sid = C.VarDecln (Just C.Static) cty name initval where
 
 -- | Define an accessor functions for the ring buffer associated with a stream
 mkaccessdecln :: Id -> Type a -> [a] -> C.FunDef
-mkaccessdecln sid ty xs = C.FunDef cty name params [] [C.Return (Just expr)] where
-  cty        = C.decay $ transtype ty
-  name       = streamaccessorname sid
-  bufflength = C.LitInt $ fromIntegral $ length xs
-  params     = [C.Param (C.TypeSpec $ C.TypedefName "size_t") "x"]
-  index      = (C.Ident (indexname sid) C..+ C.Ident "x") C..% bufflength
-  expr       = C.Index (C.Ident (streamname sid)) index
+mkaccessdecln sid ty xs = C.FunDef cty name params [] [C.Return (Just expr)]
+  where
+    cty        = C.decay $ transtype ty
+    name       = streamaccessorname sid
+    bufflength = C.LitInt $ fromIntegral $ length xs
+    params     = [C.Param (C.TypeSpec $ C.TypedefName "size_t") "x"]
+    index      = (C.Ident (indexname sid) C..+ C.Ident "x") C..% bufflength
+    expr       = C.Index (C.Ident (streamname sid)) index
 
 -- | Writes the step function, that updates all streams.
 mkstep :: CSettings -> [Stream] -> [Trigger] -> [External] -> C.FunDef
