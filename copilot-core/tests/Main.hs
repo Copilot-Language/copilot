03d9250bc7ce1409839f3c7f9242e03f5db91954
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-09-21 16:22:22 -0400

    Unit testing of copilot-core:Copilot.Core.External. Refs #256.
    This commit introduces tests for the Copilot.Core.External module,
    checking that the function externVars is able to find the extern
    variables introduced in a specification.
    
    Tests are hard-coded and could potentially be generated using a
    randomized approach, but the current implementation does not do that.

--- copilot-core/tests/Main.hs
+++ copilot-core/tests/Main.hs
@@ -7,6 +7,7 @@ import Test.Framework (Test, defaultMainWithOpts)
 
 -- Internal library modules being tested
 import qualified Test.Copilot.Core.Error
+import qualified Test.Copilot.Core.External
 
 -- | Run all unit tests on copilot-core.
 main :: IO ()
@@ -16,4 +17,5 @@ main = defaultMainWithOpts tests mempty
 tests :: [Test.Framework.Test]
 tests =
   [ Test.Copilot.Core.Error.tests
+  , Test.Copilot.Core.External.tests
   ]
