17cd3f2efa48523ca9b22e9f71c0f22dc0947687
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-01-12 18:56:05 +0100

    Replace exposed modules with old internal implementations. Refs #289.
    In a prior commit, internal implementations had been moved to a separate
    module so that modules that were not going to be exposed directly could
    be deprecated safely without triggering warnings when compiling
    copilot-c99.
    
    With the concealment of the library's implementation, those additional
    internal modules can return to where they were. This commit renames the
    modules to have their original names, adjusts imports, and the cabal
    file so that the modules removed are no longer listed in the library.

--- copilot-c99/src/Copilot/Compile/C99/Settings.hs
+++ copilot-c99/src/Copilot/Compile/C99/Settings.hs
@@ -1,8 +1,12 @@
 -- | Settings used by the code generator to customize the code.
-module Copilot.Compile.C99.Settings
-  {-# DEPRECATED "This module will be hidden in future versions." #-}
-  (module Copilot.Compile.C99.Settings.Internal)
- where
+module Copilot.Compile.C99.Settings where
 
-import Copilot.Compile.C99.Settings.Internal
+-- | Settings used to customize the code generated.
+data CSettings = CSettings
+  { cSettingsStepFunctionName :: String
+  , cSettingsOutputDirectory  :: FilePath
+  }
 
+-- | Default settings with a step function called @step@.
+mkDefaultCSettings :: CSettings
+mkDefaultCSettings = CSettings "step" "."
