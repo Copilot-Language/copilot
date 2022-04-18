ef559642f6952b00298f24cae4a4303962bf281d
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-04-18 16:05:27 -0400

    Remove test for copilot-core:Copilot.Core.Error. Refs #300.
    The module copilot-core:Copilot.Core.Error only contains functions that
    were deprecated in Copilot 3.6. Those functions should not be exposed,
    since they serve no purpose outside of the internals of the
    implementation.
    
    As per our internal policy of waiting 3 versions from deprecation until
    a public interface declaration can be removed, the complete module can
    now be hidden.
    
    Once the module is hidden, the tests that apply to that part of the
    public interface of that module are no longer needed.
    
    This commit removes those tests as a preliminary step for removing the
    module itself. It also removes auxiliary operations that were only being
    used by those tests.

--- copilot-core/tests/Main.hs
+++ copilot-core/tests/Main.hs
@@ -6,7 +6,6 @@ import Data.Monoid    (mempty)
 import Test.Framework (Test, defaultMainWithOpts)
 
 -- Internal library modules being tested
-import qualified Test.Copilot.Core.Error
 import qualified Test.Copilot.Core.External
 import qualified Test.Copilot.Core.Interpret.Eval
 import qualified Test.Copilot.Core.Type
@@ -21,8 +20,7 @@ main = defaultMainWithOpts tests mempty
 -- | All unit tests in copilot-core.
 tests :: [Test.Framework.Test]
 tests =
-  [ Test.Copilot.Core.Error.tests
-  , Test.Copilot.Core.External.tests
+  [ Test.Copilot.Core.External.tests
   , Test.Copilot.Core.Interpret.Eval.tests
   , Test.Copilot.Core.Type.tests
   , Test.Copilot.Core.Type.Array.tests
