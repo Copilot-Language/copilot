d41a6f1f47d509b0a5d707810bf7bdbff22748b5
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-04-25 17:03:30 -0400

    Style: squash multiple consecutive empty lines. Refs #316.
    According to our style guide, there should only be one empty line (at most)
    between two lines of code..

--- copilot-libraries/src/Copilot/Library/PTLTL.hs
+++ copilot-libraries/src/Copilot/Library/PTLTL.hs
@@ -27,18 +27,15 @@ import Copilot.Language
 previous :: Stream Bool -> Stream Bool
 previous s = [ False ] ++ s
 
-
 -- | Has @s@ always held (up to and including the current state)?
 alwaysBeen :: Stream Bool -> Stream Bool
 alwaysBeen s = s && tmp
     where tmp = [ True ] ++ s && tmp
 
-
 -- | Did @s@ hold at some time in the past (including the current state)?
 eventuallyPrev :: Stream Bool -> Stream Bool
 eventuallyPrev s = s || tmp
   where tmp = [ False ] ++ s || tmp
-
 
 -- | Once @s2@ holds, in the following state (period), does @s1@ continuously hold?
 since ::  Stream Bool -> Stream Bool -> Stream Bool
