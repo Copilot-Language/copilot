b9864edf1efba10c18d06effeb00a3d3bfa7605f
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-04-25 17:06:36 -0400

    Style: remove separating line in comment. Refs #316.
    According to our style guide, the code should not use lines with multiple
    consecutive hyphens, stars, or other symbols, to separate sections of code.

--- copilot-theorem/src/Copilot/Theorem/Kind2/Output.hs
+++ copilot-theorem/src/Copilot/Theorem/Kind2/Output.hs
@@ -1,5 +1,3 @@
---------------------------------------------------------------------------------
-
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE Safe       #-}
 
@@ -11,8 +9,6 @@ import Copilot.Theorem.Prove  as P
 import Data.Maybe           (fromJust)
 
 import qualified Copilot.Theorem.Misc.Error as Err
-
---------------------------------------------------------------------------------
 
 simpleName s = QName s Nothing Nothing
 
@@ -52,5 +48,3 @@ parseOutput prop xml = fromJust $ do
     err msg = Err.fatal $
       "Parse error while reading the Kind2 XML output : \n"
       ++ msg ++ "\n\n" ++ xml
-
---------------------------------------------------------------------------------
