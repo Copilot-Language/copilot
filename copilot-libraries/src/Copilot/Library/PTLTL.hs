f29e48a8a9aaabf6ae3b6eb78d2dad370aa822c2
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-04-25 17:37:51 -0400

    Style: make where keyword stand alone on one line. Refs #316.
    According to our style guide, the where keyword should be alone on one line
    (that is, neither following an expression nor with a definition following it),
    except in data/type class/instance/module/type family declarations.

--- copilot-libraries/src/Copilot/Library/PTLTL.hs
+++ copilot-libraries/src/Copilot/Library/PTLTL.hs
@@ -30,14 +30,17 @@ previous s = [ False ] ++ s
 -- | Has @s@ always held (up to and including the current state)?
 alwaysBeen :: Stream Bool -> Stream Bool
 alwaysBeen s = s && tmp
-    where tmp = [ True ] ++ s && tmp
+    where
+      tmp = [ True ] ++ s && tmp
 
 -- | Did @s@ hold at some time in the past (including the current state)?
 eventuallyPrev :: Stream Bool -> Stream Bool
 eventuallyPrev s = s || tmp
-  where tmp = [ False ] ++ s || tmp
+  where
+    tmp = [ False ] ++ s || tmp
 
 -- | Once @s2@ holds, in the following state (period), does @s1@ continuously hold?
 since ::  Stream Bool -> Stream Bool -> Stream Bool
 since s1 s2 = alwaysBeen ( tmp ==> s1 )
-    where tmp = eventuallyPrev $ [ False ] ++ s2
+    where
+      tmp = eventuallyPrev $ [ False ] ++ s2
