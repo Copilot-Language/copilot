8a7eb13c85f8b27858cbdf6c5b579db9b4789f0f
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-04-25 17:38:38 -0400

    Style: make where keyword stand alone on one line. Refs #316.
    According to our style guide, the where keyword should be alone on one line
    (that is, neither following an expression nor with a definition following it),
    except in data/type class/instance/module/type family declarations.

--- copilot-libraries/src/Copilot/Library/Statistics.hs
+++ copilot-libraries/src/Copilot/Library/Statistics.hs
@@ -22,12 +22,14 @@ sum n s = nfoldl1 n (+) s
 -- | Maximum value.
 max :: ( Typed a, Ord a ) => Int -> Stream a -> Stream a
 max n s = nfoldl1 n largest s
-    where largest  = \ x y -> mux ( x >= y ) x y
+    where
+      largest  = \ x y -> mux ( x >= y ) x y
 
 -- | Minimum value.
 min :: ( Typed a, Ord a ) => Int -> Stream a -> Stream a
 min n s = nfoldl1 n smallest s
-    where smallest = \ x y -> mux ( x <= y ) x y
+    where
+      smallest = \ x y -> mux ( x <= y ) x y
 
 -- | Mean value.  @n@ must not overflow
 -- for word size @a@ for streams over which computation is peformed.
