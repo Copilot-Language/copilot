536ee5a67d1f4c3956b19aeef4500e7f786be1d2
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-04-25 17:03:30 -0400

    Style: squash multiple consecutive empty lines. Refs #316.
    According to our style guide, there should only be one empty line (at most)
    between two lines of code..

--- copilot-libraries/src/Copilot/Library/LTL.hs
+++ copilot-libraries/src/Copilot/Library/LTL.hs
@@ -45,7 +45,6 @@ module Copilot.Library.LTL
 import Copilot.Language
 import Copilot.Library.Utils
 
-
 -- | Property @s@ holds at the next period.  For example:
 --
 -- @
@@ -56,7 +55,6 @@ import Copilot.Library.Utils
 -- Note: @s@ must have sufficient history to 'drop' a value from it.
 next :: Stream Bool -> Stream Bool
 next = drop ( 1 :: Int )
-
 
 -- | Property @s@ holds for the next @n@ periods.  We require @n >= 0@. If @n ==
 -- 0@, then @s@ holds in the current period, e.g., if @p = always 2 s@, then we
@@ -71,7 +69,6 @@ next = drop ( 1 :: Int )
 -- Note: @s@ must have sufficient history to 'drop' @n@ values from it.
 always :: ( Integral a ) => a -> Stream Bool -> Stream Bool
 always n = nfoldl1 ( fromIntegral n + 1 ) (&&)
-
 
 -- | Property @s@ holds at some period in the next @n@ periods.  If @n == 0@,
 -- then @s@ holds in the current period.  We require @n >= 0@.  E.g., if @p =
@@ -90,7 +87,6 @@ eventually :: ( Integral a ) =>
               -> Stream Bool
 eventually n = nfoldl1 ( fromIntegral n + 1 ) (||)
 
-
 -- | @until n s0 s1@ means that @eventually n s1@, and up until at least the
 -- period before @s1@ holds, @s0@ continuously holds.
 --
@@ -103,7 +99,6 @@ until n s0 s1 = foldl (||) s1 v0
           v0 = [ always ( i :: Int ) s0 && drop ( i + 1 ) s1
                | i <- [ 0 .. n' - 1 ]
                ]
-
 
 -- | @release n s0 s1@ means that either @always n s1@, or @s1@ holds up to and
 -- including the period at which @s0@ becomes true.
