8235d8317636dbf650423088f83a3e1ccd204f61
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-11-03 12:53:08 -0700

    copilot-core: Break lines at 80 characters in Copilot.Core.Expr. Refs #332.
    This commit adds line breaks in the signatures of data constructors in the
    module Copilot.Core.Expr. For consistency and readability, we do it also for
    constructors of the same type for which the complete signature would fit in 80
    characters, since they all have a recurrent pattern.

--- copilot-core/src/Copilot/Core/Expr.hs
+++ copilot-core/src/Copilot/Core/Expr.hs
@@ -37,15 +37,32 @@ type DropIdx = Word32
 -- representation contains information about the types of elements in the
 -- stream.
 data Expr a where
-  Const     :: Typeable a => Type a -> a -> Expr a
-  Drop      :: Typeable a => Type a -> DropIdx -> Id -> Expr a
-  Local     :: Typeable a => Type a -> Type b -> Name -> Expr a -> Expr b -> Expr b
-  Var       :: Typeable a => Type a -> Name -> Expr a
-  ExternVar :: Typeable a => Type a -> Name -> Maybe [a] -> Expr a
-  Op1       :: Typeable a => Op1 a b -> Expr a -> Expr b
-  Op2       :: (Typeable a, Typeable b) => Op2 a b c -> Expr a -> Expr b -> Expr c
-  Op3       :: (Typeable a, Typeable b, Typeable c) => Op3 a b c d -> Expr a -> Expr b -> Expr c -> Expr d
-  Label     :: Typeable a => Type a -> String -> Expr a -> Expr a
+  Const     :: Typeable a
+            => Type a -> a -> Expr a
+
+  Drop      :: Typeable a
+            => Type a -> DropIdx -> Id -> Expr a
+
+  Local     :: Typeable a
+            => Type a -> Type b -> Name -> Expr a -> Expr b -> Expr b
+
+  Var       :: Typeable a
+            => Type a -> Name -> Expr a
+
+  ExternVar :: Typeable a
+            => Type a -> Name -> Maybe [a] -> Expr a
+
+  Op1       :: Typeable a
+            => Op1 a b -> Expr a -> Expr b
+
+  Op2       :: (Typeable a, Typeable b)
+            => Op2 a b c -> Expr a -> Expr b -> Expr c
+
+  Op3       :: (Typeable a, Typeable b, Typeable c)
+            => Op3 a b c d -> Expr a -> Expr b -> Expr c -> Expr d
+
+  Label     :: Typeable a
+            => Type a -> String -> Expr a -> Expr a
 
 -- | A untyped expression that carries the information about the type of the
 -- expression as a value, as opposed to exposing it at type level (using an
