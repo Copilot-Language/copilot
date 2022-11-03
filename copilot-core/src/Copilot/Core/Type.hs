5682386ddffd5f5a4bc951cfee94b5f356febb97
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-11-03 12:25:34 -0700

    copilot-core: Adjust horizontal space in Copilot.Core.Type. Refs #332.
    This commit adjusts the horizontal space in Copilot.Core.Type so no extra space
    is added unless needed (e.g., to align elements).

--- copilot-core/src/Copilot/Core/Type.hs
+++ copilot-core/src/Copilot/Core/Type.hs
@@ -64,19 +64,19 @@ class Struct a where
   toValues :: a -> [Value a]
 
 -- | The field of a struct, together with a representation of its type.
-data Value a = forall s t. (Typeable t, KnownSymbol s, Show t) => Value (Type t) (Field s t)
+data Value a = forall s t . (Typeable t, KnownSymbol s, Show t) => Value (Type t) (Field s t)
 
 -- | A field in a struct. The name of the field is a literal at the type
 -- level.
 data Field (s :: Symbol) t = Field t
 
 -- | Extract the name of a field.
-fieldname :: forall s t. KnownSymbol s => Field s t -> String
+fieldname :: forall s t . KnownSymbol s => Field s t -> String
 fieldname _ = symbolVal (Proxy :: Proxy s)
 
 -- | Extract the name of an accessor (a function that returns a field of a
 -- struct).
-accessorname :: forall a s t. (Struct a, KnownSymbol s) => (a -> Field s t) -> String
+accessorname :: forall a s t . (Struct a, KnownSymbol s) => (a -> Field s t) -> String
 accessorname _ = symbolVal (Proxy :: Proxy s)
 
 instance (KnownSymbol s, Show t) => Show (Field s t) where
@@ -95,28 +95,28 @@ instance {-# OVERLAPPABLE #-} (Typed t, Struct t) => Show t where
 -- former, the length of the array is part of the type. In the latter, the
 -- names of the fields are part of the type.
 data Type :: * -> * where
-  Bool    :: Type Bool
-  Int8    :: Type Int8
-  Int16   :: Type Int16
-  Int32   :: Type Int32
-  Int64   :: Type Int64
-  Word8   :: Type Word8
-  Word16  :: Type Word16
-  Word32  :: Type Word32
-  Word64  :: Type Word64
-  Float   :: Type Float
-  Double  :: Type Double
-  Array   :: forall n t. ( KnownNat n
+  Bool   :: Type Bool
+  Int8   :: Type Int8
+  Int16  :: Type Int16
+  Int32  :: Type Int32
+  Int64  :: Type Int64
+  Word8  :: Type Word8
+  Word16 :: Type Word16
+  Word32 :: Type Word32
+  Word64 :: Type Word64
+  Float  :: Type Float
+  Double :: Type Double
+  Array  :: forall n t . ( KnownNat n
                          , Typed t
                          ) => Type t -> Type (Array n t)
-  Struct  :: (Typed a, Struct a) => a -> Type a
+  Struct :: (Typed a, Struct a) => a -> Type a
 
 -- | Return the length of an array from its type
-tylength :: forall n t. KnownNat n => Type (Array n t) -> Int
+tylength :: forall n t . KnownNat n => Type (Array n t) -> Int
 tylength _ = fromIntegral $ natVal (Proxy :: Proxy n)
 
 -- | Return the total (nested) size of an array from its type
-tysize :: forall n t. KnownNat n => Type (Array n t) -> Int
+tysize :: forall n t . KnownNat n => Type (Array n t) -> Int
 tysize ty@(Array ty'@(Array _)) = tylength ty * tysize ty'
 tysize ty@(Array _            ) = tylength ty
 
@@ -171,20 +171,20 @@ data SimpleType where
 -- This instance is necessary, otherwise the type of SArray can't be inferred.
 
 instance Eq SimpleType where
-  SBool   == SBool    = True
-  SInt8   == SInt8    = True
-  SInt16  == SInt16   = True
-  SInt32  == SInt32   = True
-  SInt64  == SInt64   = True
-  SWord8  == SWord8   = True
-  SWord16 == SWord16  = True
-  SWord32 == SWord32  = True
-  SWord64 == SWord64  = True
-  SFloat  == SFloat   = True
-  SDouble == SDouble  = True
+  SBool   == SBool   = True
+  SInt8   == SInt8   = True
+  SInt16  == SInt16  = True
+  SInt32  == SInt32  = True
+  SInt64  == SInt64  = True
+  SWord8  == SWord8  = True
+  SWord16 == SWord16 = True
+  SWord32 == SWord32 = True
+  SWord64 == SWord64 = True
+  SFloat  == SFloat  = True
+  SDouble == SDouble = True
   (SArray t1) == (SArray t2) | Just DE.Refl <- testEquality t1 t2 = True
                              | otherwise                          = False
-  SStruct == SStruct  = True
+  SStruct == SStruct = True
   _ == _ = False
 
 -- | A typed expression, from which we can obtain the two type representations
@@ -194,22 +194,22 @@ class (Show a, Typeable a) => Typed a where
   simpleType :: Type a -> SimpleType
   simpleType _ = SStruct
 
-instance Typed Bool   where
+instance Typed Bool where
   typeOf       = Bool
   simpleType _ = SBool
-instance Typed Int8   where
+instance Typed Int8 where
   typeOf       = Int8
   simpleType _ = SBool
-instance Typed Int16  where
+instance Typed Int16 where
   typeOf       = Int16
   simpleType _ = SInt16
-instance Typed Int32  where
+instance Typed Int32 where
   typeOf       = Int32
   simpleType _ = SInt32
-instance Typed Int64  where
+instance Typed Int64 where
   typeOf       = Int64
   simpleType _ = SInt64
-instance Typed Word8  where
+instance Typed Word8 where
   typeOf       = Word8
   simpleType _ = SWord8
 instance Typed Word16 where
@@ -221,15 +221,15 @@ instance Typed Word32 where
 instance Typed Word64 where
   typeOf       = Word64
   simpleType _ = SWord64
-instance Typed Float  where
+instance Typed Float where
   typeOf       = Float
   simpleType _ = SFloat
 instance Typed Double where
   typeOf       = Double
   simpleType _ = SDouble
 instance (Typeable t, Typed t, KnownNat n) => Typed (Array n t) where
-  typeOf                = Array typeOf
-  simpleType (Array t)  = SArray t
+  typeOf               = Array typeOf
+  simpleType (Array t) = SArray t
 
 -- | A untyped type (no phantom type).
 data UType = forall a . Typeable a => UType { uTypeType :: Type a }
