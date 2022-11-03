eb7ccf503cb5d9f67f8e109f8d002258e24c166e
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-11-03 12:36:44 -0700

    copilot-core: Add extra vertical space in Copilot.Core.Type. Refs #332.
    This commit add vertical space between top-level definitions in
    Copilot.Core.Type.

--- copilot-core/src/Copilot/Core/Type.hs
+++ copilot-core/src/Copilot/Core/Type.hs
@@ -197,36 +197,47 @@ class (Show a, Typeable a) => Typed a where
 instance Typed Bool where
   typeOf       = Bool
   simpleType _ = SBool
+
 instance Typed Int8 where
   typeOf       = Int8
   simpleType _ = SBool
+
 instance Typed Int16 where
   typeOf       = Int16
   simpleType _ = SInt16
+
 instance Typed Int32 where
   typeOf       = Int32
   simpleType _ = SInt32
+
 instance Typed Int64 where
   typeOf       = Int64
   simpleType _ = SInt64
+
 instance Typed Word8 where
   typeOf       = Word8
   simpleType _ = SWord8
+
 instance Typed Word16 where
   typeOf       = Word16
   simpleType _ = SWord16
+
 instance Typed Word32 where
   typeOf       = Word32
   simpleType _ = SWord32
+
 instance Typed Word64 where
   typeOf       = Word64
   simpleType _ = SWord64
+
 instance Typed Float where
   typeOf       = Float
   simpleType _ = SFloat
+
 instance Typed Double where
   typeOf       = Double
   simpleType _ = SDouble
+
 instance (Typeable t, Typed t, KnownNat n) => Typed (Array n t) where
   typeOf               = Array typeOf
   simpleType (Array t) = SArray t
