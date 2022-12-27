a79b4fc4084ed72e10ccc4b5d62a5ac665a184a8
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-12-27 15:28:42 -0800

    copilot-core: Remove test for module Copilot.Core.External. Refs #391.
    The module Copilot.Core.External is not being used by any other part of
    Copilot. It was deprecated in Copilot 3.10 and no messages have been received
    requesting that it be kept in the implementation. As per our internal policy of
    waiting 3 versions from deprecation until a public interface declaration can be
    removed, this module can now be removed.
    
    This commit removes all tests associated to that module, so that it can
    be removed safely.

--- copilot-core/tests/Main.hs
+++ copilot-core/tests/Main.hs
@@ -5,7 +5,6 @@ module Main where
 import Test.Framework (Test, defaultMain)
 
 -- Internal library modules being tested
-import qualified Test.Copilot.Core.External
 import qualified Test.Copilot.Core.Interpret.Eval
 import qualified Test.Copilot.Core.Type
 import qualified Test.Copilot.Core.Type.Array
@@ -18,8 +17,7 @@ main = defaultMain tests
 -- | All unit tests in copilot-core.
 tests :: [Test.Framework.Test]
 tests =
-  [ Test.Copilot.Core.External.tests
-  , Test.Copilot.Core.Interpret.Eval.tests
+  [ Test.Copilot.Core.Interpret.Eval.tests
   , Test.Copilot.Core.Type.tests
   , Test.Copilot.Core.Type.Array.tests
   , Test.Copilot.Core.Type.Show.tests
