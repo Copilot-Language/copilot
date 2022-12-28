39d52cc6fc42d5ec0471e5e8ead0c765dab7ad9e
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-12-27 17:42:26 -0800

    copilot-core: Remove tests involving Copilot.Core.Type.Show. Refs #392.
    The module Copilot.Core.Type.Show is not being used outside of copilot-core. It
    was deprecated in Copilot 3.10 and no messages have been received requesting
    that it be kept in the implementation. As per our internal policy of waiting 3
    versions from deprecation until a public interface declaration can be removed,
    this module can now be completely hidden from the public interface.
    
    This commit removes all tests associated to or using that module, so that it
    can be removed safely. Tests related to the interpreter are removed as well,
    since they are also present in the copilot-interpreter package.

--- copilot-core/tests/Main.hs
+++ copilot-core/tests/Main.hs
@@ -5,10 +5,8 @@ module Main where
 import Test.Framework (Test, defaultMain)
 
 -- Internal library modules being tested
-import qualified Test.Copilot.Core.Interpret.Eval
 import qualified Test.Copilot.Core.Type
 import qualified Test.Copilot.Core.Type.Array
-import qualified Test.Copilot.Core.Type.Show
 
 -- | Run all unit tests on copilot-core.
 main :: IO ()
@@ -17,8 +15,6 @@ main = defaultMain tests
 -- | All unit tests in copilot-core.
 tests :: [Test.Framework.Test]
 tests =
-  [ Test.Copilot.Core.Interpret.Eval.tests
-  , Test.Copilot.Core.Type.tests
+  [ Test.Copilot.Core.Type.tests
   , Test.Copilot.Core.Type.Array.tests
-  , Test.Copilot.Core.Type.Show.tests
   ]
