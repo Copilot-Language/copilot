09a9ed76f779675ec10b718a7c6769b117b5ea97
Ivan Perez <ivan.perezdominguez@nasa.gov>
2023-03-04 00:58:00 +0000

    copilot-core: Remove unused definitions from Copilot.Core.Type.Array. Refs #411.
    The functions Copilot.Core.Type.Array.flatten and
    Copilot.Core.Type.Array.size, the classes
    Copilot.Core.Type.Array.Flatten and Copilot.Core.Type.Array.InnerType,
    and the Foldable instance for Array, are not being used by any other
    part of Copilot. They were deprecated in Copilot 3.11 and no messages
    have been received requesting that they be kept in the implementation.
    As per our internal policy of waiting 3 versions from deprecation until
    a public interface declaration can be removed, these definitions can now
    be removed.
    
    This commit removes those definitions from the module
    Copilot.Core.Type.Array, as well as any language extensions that were
    only needed for those features removed.

--- copilot-core/src/Copilot/Core/Type/Array.hs
+++ copilot-core/src/Copilot/Core/Type/Array.hs
@@ -1,11 +1,8 @@
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE Safe                  #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE Safe                #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
 
 -- |
 -- Copyright: (c) 2011 National Institute of Aerospace / Galois, Inc.
@@ -16,10 +13,6 @@
 module Copilot.Core.Type.Array
     ( Array
     , array
-    , flatten
-    , size
-    , Flatten
-    , InnerType
     , arrayelems
     )
   where
@@ -45,36 +38,6 @@ array xs | datalen == typelen = Array xs
     typelen = fromIntegral $ natVal (Proxy :: Proxy n)
     errmsg = "Length of data (" ++ show datalen ++
              ") does not match length of type (" ++ show typelen ++ ")."
-
--- | Association between an array and the type of the elements it contains.
-{-# DEPRECATED InnerType "This type family is deprecated in Copilot 3.11." #-}
-type family InnerType x where
-  InnerType (Array _ x) = InnerType x
-  InnerType x           = x
-
--- | Flattening or conversion of arrays to lists.
-{-# DEPRECATED Flatten "This class is deprecated in Copilot 3.11." #-}
-{-# DEPRECATED flatten "This function is deprecated in Copilot 3.11." #-}
-class Flatten a b where
-  -- | Flatten an array to a list.
-  flatten :: Array n a -> [b]
-
--- | Flattening of plain arrays.
-instance Flatten a a where
-  flatten (Array xs) = xs
-
--- | Flattening of nested arrays.
-instance Flatten a b => Flatten (Array n a) b where
-  flatten (Array xss) = concat $ map flatten xss
-
--- | This instance is deprecated in Copilot 3.11.
-instance Foldable (Array n) where
-  foldr f base (Array xs) = foldr f base xs
-
--- | Total number of elements in a possibly nested array.
-{-# DEPRECATED size "This function is deprecated in Copilot 3.11." #-}
-size :: forall a n b. (Flatten a b, b ~ InnerType a) => Array n a -> Int
-size xs = length $ (flatten xs :: [b])
 
 -- | Return the elemts of an array.
 arrayelems :: Array n a -> [a]
