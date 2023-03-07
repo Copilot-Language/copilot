213ca12c08384ce6f85e9227aab7e2bee8e4b501
Ivan Perez <ivan.perezdominguez@nasa.gov>
2023-03-07 18:51:32 +0000

    copilot-core: Remove tests related to definitions scheduled for removal. Refs #411.
    The functions Copilot.Core.Type.Array.flatten and
    Copilot.Core.Type.Array.size, the classes
    Copilot.Core.Type.Array.Flatten and Copilot.Core.Type.Array.InnerType,
    and the Foldable instance for Array, are not being used by any other
    part of Copilot. They were deprecated in Copilot 3.11 and no messages
    have been received requesting that they be kept in the implementation.
    As per our internal policy of waiting 3 versions from deprecation until
    a public interface declaration can be removed, these definitions can now
    be removed.
    
    This commit removes any tests that require those definitions that will
    be removed from Copilot.Core.Type.Array.

--- copilot-core/tests/Test/Copilot/Core/Type/Array.hs
+++ copilot-core/tests/Test/Copilot/Core/Type/Array.hs
@@ -13,7 +13,7 @@ import Test.QuickCheck                      (Gen, Property, arbitrary, forAll,
                                              vectorOf)
 
 -- Internal imports: library modules being tested
-import Copilot.Core.Type.Array (Array, array, arrayelems, flatten, size)
+import Copilot.Core.Type.Array (Array, array, arrayelems)
 
 -- | All unit tests for copilot-core:Copilot.Core.Array.
 tests :: Test.Framework.Test
@@ -25,30 +25,6 @@ tests =
         (testArrayElemsLeft (Proxy :: Proxy 5))
     , testProperty "arrayElems . array (identity; 200)"
         (testArrayElemsLeft (Proxy :: Proxy 200))
-    , testProperty "arrayFlatten . array (identity; 0)"
-        (testArrayFlattenLeft (Proxy :: Proxy 0))
-    , testProperty "arrayFlatten . array (identity; 1)"
-        (testArrayFlattenLeft (Proxy :: Proxy 1))
-    , testProperty "arrayFlatten . array (identity; 2)"
-        (testArrayFlattenLeft (Proxy :: Proxy 2))
-    , testProperty "arrayFlatten . array (identity; 22)"
-        (testArrayFlattenLeft (Proxy :: Proxy 22))
-    , testProperty "arrayFlatten . array (identity; 50)"
-        (testArrayFlattenLeft (Proxy :: Proxy 50))
-    , testProperty "arrayFlatten . array (identity; nested; nested; 2x6)"
-        (testArrayFlattenNestedLeft (Proxy :: Proxy 2) (Proxy :: Proxy 6))
-    , testProperty "arrayFlatten . array (identity; nested; nested; 10x2)"
-        (testArrayFlattenNestedLeft (Proxy :: Proxy 10) (Proxy :: Proxy 2))
-    , testProperty "arrayFlatten . array (identity; nested; nested; 38x20)"
-        (testArrayFlattenNestedLeft (Proxy :: Proxy 38) (Proxy :: Proxy 20))
-    , testProperty "arrayFlatten . array (identity; nested; nested; 125x100)"
-        (testArrayFlattenNestedLeft (Proxy :: Proxy 125) (Proxy :: Proxy 100))
-    , testProperty "arraySize . array (identity; 0)"
-        (testArraySizeLeft (Proxy :: Proxy 0))
-    , testProperty "arraySize . array (identity; 45)"
-        (testArraySizeLeft (Proxy :: Proxy 45))
-    , testProperty "arraySize . array (identity; 100)"
-        (testArraySizeLeft (Proxy :: Proxy 100))
     ]
 
 -- * Individual tests
@@ -61,69 +37,6 @@ testArrayElemsLeft len =
       let array' :: Array n Int64
           array' = array ls
       in arrayelems array' == ls
-
-  where
-
-    -- Generator for lists of Int64 of known length.
-    xsInt64 :: Gen [Int64]
-    xsInt64 = vectorOf (fromIntegral (natVal len)) arbitrary
-
--- | Test that building an array from a list and extracting the elements with
--- the function 'flatten' will result in the same list.
---
--- This test tests only plain arrays (no nesting).
-testArrayFlattenLeft :: forall n . KnownNat n => Proxy n -> Property
-testArrayFlattenLeft len =
-    forAll xsInt64 $ \ls ->
-      let array' :: Array n Int64
-          array' = array ls
-      in flatten array' == ls
-
-  where
-
-    -- Generator for lists of Int64 of known length.
-    xsInt64 :: Gen [Int64]
-    xsInt64 = vectorOf (fromIntegral (natVal len)) arbitrary
-
--- | Test that building a nested array of a known size and extracting the
--- elements with the function 'flatten' will result in a list with a size that
--- is the product of the sizes provided.
-testArrayFlattenNestedLeft :: forall n1 n2
-                           .  (KnownNat n1, KnownNat n2)
-                           => Proxy n1
-                           -> Proxy n2
-                           -> Property
-testArrayFlattenNestedLeft len1 len2 =
-    forAll xsArrays $ \array' ->
-      length (flatten array' :: [Int64]) == expectedSize
-
-  where
-
-    -- Expected size of the matrix / array.
-    expectedSize :: Int
-    expectedSize = fromIntegral $ natVal len1 * natVal len2
-
-    -- Generator for nested matrices of Int64 of known size.
-    xsArrays :: Gen (Array n1 (Array n2 Int64))
-    xsArrays =
-      array <$> vectorOf (fromIntegral (natVal len1)) xsArrayInt64
-
-    -- Generator for arrays of Int64 of known length.
-    xsArrayInt64 :: Gen (Array n2 Int64)
-    xsArrayInt64 = array <$> xsInt64
-
-    -- Generator for lists of Int64 of known length.
-    xsInt64 :: Gen [Int64]
-    xsInt64 = vectorOf (fromIntegral (natVal len2)) arbitrary
-
--- | Test that building an array from a list and calculating the size will
--- return the length of the list.
-testArraySizeLeft :: forall n . KnownNat n => Proxy n -> Property
-testArraySizeLeft len =
-    forAll xsInt64 $ \ls ->
-      let array' :: Array n Int64
-          array' = array ls
-      in size array' == length ls
 
   where
 
