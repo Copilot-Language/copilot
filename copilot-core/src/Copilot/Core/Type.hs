d68c03d9d73fa203d8220c6b409195ede047658d
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-03-08 01:39:04 +0000

    copilot-core: Remove deprecated function Copilot.Core.Type.tylength. Refs #500.
    The function Copilot.Core.Type.tylength not being used by any other part
    of Copilot. It was replaced by a function with lowerCamelCase format, in
    compliance with Copilot's style guide.  The original function was
    deprecated in Copilot 3.17. As per our internal policy of waiting 3
    versions from deprecation until a public interface declaration can be
    removed, this function can now be removed.
    
    This commit removes the function Copilot.Core.Type.tylength from
    copilot-core.

--- copilot-core/src/Copilot/Core/Type.hs
+++ copilot-core/src/Copilot/Core/Type.hs
@@ -25,7 +25,6 @@ module Copilot.Core.Type
 
     , typeSize
     , typeLength
-    , tylength
 
     , Value (..)
     , toValues
@@ -135,11 +134,6 @@ data Type :: * -> * where
 -- | Return the length of an array from its type
 typeLength :: forall n t . KnownNat n => Type (Array n t) -> Int
 typeLength _ = fromIntegral $ natVal (Proxy :: Proxy n)
-
-{-# DEPRECATED tylength "Use typeLength instead." #-}
--- | Return the length of an array from its type
-tylength :: forall n t . KnownNat n => Type (Array n t) -> Int
-tylength = typeLength
 
 -- | Return the total (nested) size of an array from its type
 typeSize :: forall n t . KnownNat n => Type (Array n t) -> Int
