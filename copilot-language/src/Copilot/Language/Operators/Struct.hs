05d616c6d65d7e228dee756dc6a53390e136314b
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-06-30 11:16:37 +0000

    copilot-language: High-level array update operator. Refs #520.
    Copilot currently supports reading values from fields of structs, but
    not changing them. We'd like to be able to modify a struct by adjusting
    the values of specific fields.
    
    A prior commit has extended copilot-core with a low-level interface
    to support struct field updates, and the C99 backend and the interpreter
    have been extended accordingly.
    
    This commit extends the high-level language definition to support struct
    field updates. We need to add an auxiliary type Projection to provide
    the notation we would like to support.
    
    Co-authored-by: Frank Dedden <dev@dedden.net>
    Co-authored-by: Ryan Scott <rscott@galois.com>

--- copilot-language/src/Copilot/Language/Operators/Struct.hs
+++ copilot-language/src/Copilot/Language/Operators/Struct.hs
@@ -1,8 +1,35 @@
 {-# LANGUAGE Safe #-}
 
 -- | Combinators to deal with streams carrying structs.
+--
+-- We support two kinds of operations on structs: reading the fields of structs
+-- and modifying the fields of structs.
+--
+-- To obtain the values of field @x@ of a struct @s@, you can just write:
+--
+-- @
+-- expr = s # x
+-- @
+--
+-- If you want to update it, use instead a double hash to refer to the field.
+-- You can either update the field:
+--
+-- @
+-- expr = s ## x =: 75
+-- @
+--
+-- To update it by applying a function to it, for example, the function that
+-- updates a stream by one unit, just do:
+--
+-- @
+-- expr = s ## x =$ (+1)
+-- @
 module Copilot.Language.Operators.Struct
   ( (#)
+  , Projection
+  , (##)
+  , (=:)
+  , (=$)
   ) where
 
 import Copilot.Core.Type
@@ -18,6 +45,46 @@ import GHC.TypeLits             (KnownSymbol)
 -- of type @Word8@, and @s@ is a stream of type @Stream T@, then @s # t2@ has
 -- type @Stream Word8@ and contains the values of the @t2@ field of the structs
 -- in @s@ at any point in time.
-(#) :: (KnownSymbol s, Typed t, Typed a, Struct a)
-      => Stream a -> (a -> Field s t) -> Stream t
+(#) :: (KnownSymbol f, Typed t, Typed s, Struct s)
+      => Stream s -> (s -> Field f t) -> Stream t
 (#) s f = Op1 (GetField typeOf typeOf f) s
+
+-- | Type represented an unapplied struct field accessor and a stream carrying
+-- structs.
+data Projection s f t = Projection (Stream s) (s -> Field f t)
+
+-- | Pair a stream with a field accessor, without applying it to obtain the
+-- value of the field.
+--
+-- This function is needed to refer to a field accessor when the goal is to
+-- update the field value, not just to read it.
+(##) :: (KnownSymbol f, Typed t, Typed s, Struct s)
+     => Stream s -> (s -> Field f t) -> Projection s f t
+(##) = Projection
+
+-- | Create a stream where the field of a struct has been updated with values
+-- from another stream.
+--
+-- For example, if a struct of type @T@ has two fields, @t1@ of type @Int32@
+-- and @t2@ of type @Word8@, and @s@ is a stream of type @Stream T@, and $sT1$
+-- is a stream of type @Int32@ then @s ## t2 =: sT1@ has type @Stream T@ and
+-- contains structs where the value of @t1@ is that of @sT1@ and the value of
+-- @t2@ is the value that the same field had in @s@, at any point in time.
+infixl 8 =:
+(=:) :: (KnownSymbol f, Typed t, Typed s, Struct s)
+     => Projection s f t -> Stream t -> Stream s
+(=:) (Projection s f) v = Op2 (UpdateField typeOf typeOf f) s v
+
+-- | Create a stream where the field of a struct has been updated by applying a
+-- function to it.
+--
+-- For example, if a struct of type @T@ has two fields, @t1@ of type @Int32@
+-- and @t2@ of type @Word8@, and @s@ is a stream of type @Stream T@, and $f$ is
+-- a function from @Stream Int32 -> Stream Int32@ then @s ## t2 =$ f@ has type
+-- @Stream T@ and contains structs where the value of @t1@ is that of @f@
+-- applied to the original value of @t1@ in @s@, and the value of @t2@ is the
+-- value that the same field had in @s@, at any point in time.
+infixl 8 =$
+(=$) :: (KnownSymbol f, Typed t, Typed s, Struct s)
+     => Projection s f t -> (Stream t -> Stream t) -> Stream s
+(=$) (Projection s f) op = s ## f =: (op (s # f))
