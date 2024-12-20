5ac80921bc95846804006a64699070d7145d0e1a
Frank Dedden <frank@systemf.dev>
2024-12-20 08:39:48 +0100

    copilot-c99: Add specific `Trigger` representation. Refs #296.
    The current implementation of Copilot does not allow using the same
    trigger handler name multiple times, since the C99 backend produces
    multiple repeated declarations, which is invalid. Consequently, the code
    generator assumes that trigger names are unique.
    
    To allow for multiple triggers that trigger the same handler, we need to
    introduce a separate construct in triggers that acts as a unique
    identifier.
    
    This commit introduces an internal datatype to represent a `Trigger`
    with a unique name that is different from that of the function called
    when triggered.

---
+++ copilot-c99/src/Copilot/Compile/C99/Representation.hs
@@ -0,0 +1,21 @@
+-- | C99 backend specific versions of selected `Copilot.Core` datatypes.
+module Copilot.Compile.C99.Representation
+    ( UniqueTrigger (..)
+    , UniqueTriggerId
+    , mkUniqueTriggers
+    )
+  where
+
+import Copilot.Core ( Trigger (..) )
+
+-- | Internal unique name for a trigger.
+type UniqueTriggerId = String
+
+-- | A `Copilot.Core.Trigger` with an unique name.
+data UniqueTrigger = UniqueTrigger UniqueTriggerId Trigger
+
+-- | Given a list of triggers, make their names unique.
+mkUniqueTriggers :: [Trigger] -> [UniqueTrigger]
+mkUniqueTriggers ts = zipWith mkUnique ts [0..]
+  where
+    mkUnique t@(Trigger name _ _) n = UniqueTrigger (name ++ "_" ++ show n) t
