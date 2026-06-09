5de4a7bb82e9671b7961b8b0b4c222e0738a1bc3
Chris Hathhorn <chathhorn@galois.com>
2026-06-09 13:53:43 -0500

    copilot-theorem: Extend range of versions of Kind2. Refs #734.
    In order to keep Copilot effectively working in the current software
    ecosystem, we need to extend the versions of dependencies that Copilot
    can be installed with. kind2 has seen release 2.3.0, but copilot-theorem
    requires kind2 0.7.2.
    
    This commit updates the copilot-theorem Kind2 native-format backend to
    produce output targeting modern releases of Kind2. It has been tested
    with Kind2 v2.3, the latest at the time of this commit.

--- copilot-theorem/src/Copilot/Theorem/Kind2/Output.hs
+++ copilot-theorem/src/Copilot/Theorem/Kind2/Output.hs
@@ -4,66 +4,97 @@
 -- | Parse output of Kind2.
 module Copilot.Theorem.Kind2.Output (parseOutput) where
 
-import Text.XML.Light       hiding (findChild)
-import Copilot.Theorem.Prove  as P
-import Data.Maybe           (fromJust)
+import Data.List.Extra (isInfixOf, splitOn, stripInfix, trim)
+import Data.Maybe      (mapMaybe)
+
+import Copilot.Theorem.Prove as P
 
 import qualified Copilot.Core as C
 
 import qualified Copilot.Theorem.Misc.Error as Err
 
-simpleName s = QName s Nothing Nothing
-
 -- | Parse output of Kind2.
+--
+-- The output is expected to be in XML format, as produced by Kind2's @-xml@
+-- flag and documented at
+-- <https://kind.cs.uiowa.edu/kind2_user_doc/3_output/2_machine_readable.html>.
+-- Note that this function does not parse the output as XML: when given a
+-- system in the native transition system format, Kind2 produces output that
+-- is not always well-formed XML (e.g., counterexamples to invariant
+-- properties cannot be printed for systems in that format -- the
+-- counterexample printing functions in Kind2's @src/inputSystem.ml@ fail with
+-- an internal error for native input -- and Kind2 reports the error in the
+-- middle of the XML output). Instead, this function looks for the
+-- @\<Property\>@ tag for the given property and extracts the answers in the
+-- @\<Answer\>@ tags it contains.
 parseOutput :: String    -- ^ Property whose validity is being checked.
             -> C.Prop    -- ^ The property's quantifier.
             -> String    -- ^ XML output of Kind2
             -> P.Output
-parseOutput propId propQuantifier xml = fromJust $ do
-  root <- parseXMLDoc xml
-  case findAnswer . findPropTag $ root of
-    "valid"       -> case propQuantifier of
-                       -- We encode a universally quantified property P as
-                       -- ∀x.P(x) in Kind2, so the original property is valid
-                       -- iff the Kind2 property is valid.
-                       C.Forall {} -> return (Output Valid   [])
-                       -- We encode an existentially quantified property P as
-                       -- ¬(∀x.¬(P(x))) in Kind2, so the original property is
-                       -- invalid iff the Kind2 property is valid.
-                       C.Exists {} -> return (Output Invalid [])
-    "falsifiable" -> case propQuantifier of
-                       -- We encode a universally quantified property P as
-                       -- ∀x.P(x) in Kind2, so the original property is invalid
-                       -- iff the Kind2 property is invalid.
-                       C.Forall {} -> return (Output Invalid [])
-                       -- We encode an existentially quantified property P as
-                       -- ¬(∀x.¬(P(x))) in Kind2, so the original property is
-                       -- valid iff the Kind2 property is invalid.
-                       C.Exists {} -> return (Output Valid   [])
-    s             -> err $ "Unrecognized status : " ++ s
+parseOutput propId propQuantifier xml
+    | "valid"       `elem` answers = quantified (Output Valid   [])
+                                                (Output Invalid [])
+    | "falsifiable" `elem` answers = quantified (Output Invalid [])
+                                                (Output Valid   [])
+    | "unknown"     `elem` answers = Output Unknown []
+    | null answers = err $ "Answer for property " ++ propId ++ " not found"
+    | otherwise    = err $ "Unrecognized status : " ++ unwords answers
 
   where
 
-    searchForRuntimeError = undefined
+    -- Pick an output based on the quantifier of the property. The first
+    -- argument is returned when the property Kind2 was asked about is valid,
+    -- and the second one when it is invalid.
+    --
+    -- We encode a universally quantified property P as ∀x.P(x) in Kind2, so
+    -- the original property is valid iff the Kind2 property is valid.
+    --
+    -- We encode an existentially quantified property P as ¬(∀x.¬(P(x))) in
+    -- Kind2, so the original property is valid iff the Kind2 property is
+    -- invalid.
+    quantified ifValid ifInvalid = case propQuantifier of
+      C.Forall {} -> ifValid
+      C.Exists {} -> ifInvalid
 
-    findPropTag root =
-      let rightElement elt =
-            qName (elName elt) == "Property"
-            && lookupAttr (simpleName "name") (elAttribs elt)
-                == Just propId
-      in case filterChildren rightElement root of
-           tag : _ -> tag
-           _ -> err $ "Tag for property " ++ propId ++ " not found"
+    -- All the answers reported for the property, in the order in which they
+    -- appear in the output. A conclusive answer (valid or falsifiable), if
+    -- any, takes precedence over inconclusive (unknown) ones, which Kind2 may
+    -- also report when the analysis terminates without an answer for all
+    -- properties.
+    answers = mapMaybe answerText
+            $ filter isRightProperty
+            $ propertyElems xml
 
-    findAnswer tag =
-      case findChildren (simpleName "Answer") tag of
-        answTag : _ ->
-          case onlyText (elContent answTag) of
-            answ : _ -> cdData answ
-            _ -> err "Invalid 'Answer' attribute"
-        _ -> err "Attribute 'Answer' not found"
+    -- Substrings of the output following a @\<Property @ opening tag, each
+    -- extending to the next such tag (or to the end of the output).
+    propertyElems = drop 1 . splitOn "<Property "
+
+    -- True if the given property element has the name of the property this
+    -- function looks for.
+    isRightProperty elem' =
+      ("name=\"" ++ escapeAttr propId ++ "\"") `isInfixOf` openingTag
+      where
+        openingTag = takeWhile (/= '>') elem'
+
+    -- The contents of the first @\<Answer\>@ tag in the given element, if
+    -- any.
+    answerText elem' = do
+      (_, rest)  <- stripInfix "<Answer" elem'
+      (_, rest') <- stripInfix ">" rest
+      let answer = trim $ takeWhile (/= '<') rest'
+      if null answer then Nothing else Just answer
 
     err :: forall a . String -> a
     err msg = Err.fatal $
       "Parse error while reading the Kind2 XML output : \n"
       ++ msg ++ "\n\n" ++ xml
+
+-- | Escape a string for use as an XML attribute value.
+escapeAttr :: String -> String
+escapeAttr = concatMap escapeChar
+  where
+    escapeChar '&' = "&amp;"
+    escapeChar '<' = "&lt;"
+    escapeChar '>' = "&gt;"
+    escapeChar '"' = "&quot;"
+    escapeChar c   = [c]
--- copilot-theorem/src/Copilot/Theorem/Kind2/PrettyPrint.hs
+++ copilot-theorem/src/Copilot/Theorem/Kind2/PrettyPrint.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE Safe #-}
 
--- | Pretty print a Kind2 file defining predicates and propositions.
+-- | Pretty print a Kind2 file defining nodes and propositions, in the native
+-- transition system input format supported by Kind2 1.0 and newer (see
+-- "Copilot.Theorem.Kind2.AST" for pointers to references on the format).
 module Copilot.Theorem.Kind2.PrettyPrint ( prettyPrint ) where
 
 import Copilot.Theorem.Misc.SExpr
@@ -14,6 +16,14 @@ type SSExpr = SExpr String
 
 -- | Reserved keyword prime.
 kwPrime = "prime"
+
+-- | Dummy position attached to the properties of the file.
+--
+-- Kind2 requires a position (in the format @file:row-col@) for properties
+-- declared with the @:user@ source annotation, and reports that position back
+-- in its output.
+propPosition :: String
+propPosition = "copilot:1-1"
 
 -- | Pretty print a Kind2 file.
 prettyPrint :: File -> String
@@ -29,30 +39,41 @@ shouldIndent (List [Atom a, Atom _])    = a `notElem` [kwPrime]
 shouldIndent _                          = True
 
 -- | Convert a file into a sequence of expressions.
+--
+-- The top node is printed last, since Kind2 analyzes the last node of the
+-- file as the top system. The properties of the file are attached to it.
 ppFile :: File -> [SSExpr]
-ppFile (File preds props) = map ppPredDef preds ++ ppProps props
+ppFile (File nodes top props) =
+  map (`ppNode` []) nodes ++ [ppNode top (ppProps props)]
 
--- | Convert a sequence of propositions into command to check each of them.
+-- | Convert a sequence of propositions into a props field.
 ppProps :: [Prop] -> [SSExpr]
-ppProps ps = [ node "check-prop" [ list $ map ppProp ps ] ]
+ppProps [] = []
+ppProps ps = [ node "props" [ list $ map ppProp ps ] ]
 
 -- | Convert a proposition into an expression.
 ppProp :: Prop -> SSExpr
-ppProp (Prop n t) = list [atom n, ppTerm t]
+ppProp (Prop n t) = list [atom n, ppTerm t, atom ":user", atom propPosition]
 
--- | Convert a predicate into an expression.
-ppPredDef :: PredDef -> SSExpr
-ppPredDef pd =
-  list [ atom "define-pred"
-       , atom (predId pd)
-       , list . map ppStateVarDef . predStateVars $ pd
-       , node "init"  [ppTerm $ predInit  pd]
-       , node "trans" [ppTerm $ predTrans pd] ]
+-- | Convert a node, together with optional extra fields, into an expression.
+ppNode :: Node -> [SSExpr] -> SSExpr
+ppNode n extraFields =
+  list $ [ atom "define-node"
+         , atom (nodeId n)
+         , list . map ppStateVarDef . nodeStateVars $ n
+         , node "init"  [ppTerm $ nodeInit  n]
+         , node "trans" [ppTerm $ nodeTrans n] ]
+         ++ extraFields
 
 -- | Convert a state variable definition into an expression.
 ppStateVarDef :: StateVarDef -> SSExpr
 ppStateVarDef svd =
-  list [atom (varId svd), ppType (varType svd)]
+  list $ [atom (varId svd), ppType (varType svd)]
+         ++ map ppStateVarFlag (varFlags svd)
+
+-- | Convert a state variable option into an expression.
+ppStateVarFlag :: StateVarFlag -> SSExpr
+ppStateVarFlag FConst = atom ":const"
 
 -- | Convert a type into an expression.
 ppType :: Type -> SSExpr
